6d34bcd19b05a5562192ff72385a23c8032f878a
Evan Dancer <evancdancer@gmail.com>
2018-07-04 13:49:29 -0700

    Add dates for calendar

---
+++ lib/squeeze_web/views/calendar_view.ex
@@ -0,0 +1,54 @@
+defmodule SqueezeWeb.CalendarView do
+  use SqueezeWeb, :view
+
+  def today do
+    Date.utc_today()
+  end
+
+  def days_in_month do
+    today()
+    |> Date.days_in_month()
+  end
+
+  def weeks_in_month do
+    days_in_month() / 7
+  end
+
+  def first_of_month do
+    today = today()
+    {:ok, date} = Date.new(today.year, today.month, 1)
+    date
+  end
+
+  def last_day_of_month do
+    today = today()
+    {:ok, date} = Date.new(today.year, today.month, Date.days_in_month(today))
+    date
+  end
+
+  def dates do
+    Date.range(first_date(), last_date())
+  end
+
+  # 7 is Sunday in elxir
+  def first_date() do
+    date = first_of_month()
+    day_of_week = Date.day_of_week(date)
+    if day_of_week == 7 do
+      date
+    else
+     Date.add(date, -day_of_week)
+    end
+  end
+
+  # Last day is 6 Saturday
+  def last_date() do
+    date = last_day_of_month()
+    day_of_week = Date.day_of_week(date)
+    if day_of_week == 7 do
+      Date.add(date, 6)
+    else
+      Date.add(date, 6 - day_of_week)
+    end
+  end
+end
--- lib/squeeze_web/views/sidebar_view.ex
+++ lib/squeeze_web/views/sidebar_view.ex
@@ -4,4 +4,22 @@ defmodule SqueezeWeb.SidebarView do
   def full_name(user) do
     "#{user.first_name} #{user.last_name}"
   end
+
+  def goal_time(goal) do
+    duration = goal.duration
+    hours = div(duration, 3600)
+    minutes = div((duration - hours * 3600), 60)
+    seconds = rem(duration, 60)
+    "#{hours}:#{minutes}:#{seconds}"
+  end
+
+  def goal_pace(goal) do
+    # min / mile
+    miles = goal.distance / 1609
+    pace = round(goal.duration / miles)
+    minutes = div(pace, 60)
+    seconds = rem(pace, 60)
+    secs = if seconds < 10, do: "0#{seconds}", else: seconds
+    "#{minutes}:#{secs}/mi"
+  end
 end
