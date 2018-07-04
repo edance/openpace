cd8ab2489029940b7eadbde369054ab4ab920c86
Evan Dancer <evancdancer@gmail.com>
2018-07-04 14:29:36 -0700

    Add dynamic calendar based on date query param

--- lib/squeeze_web/controllers/dashboard_controller.ex
+++ lib/squeeze_web/controllers/dashboard_controller.ex
@@ -3,11 +3,23 @@ defmodule SqueezeWeb.DashboardController do
 
   alias Squeeze.Dashboard
 
-  def index(conn, _params) do
+  def index(conn, params) do
     user = conn.assigns.current_user
     conn
+    |> assign(:date, parse_date(params["date"]))
     |> assign(:goal, Dashboard.get_current_goal(user))
     |> assign(:activities, Dashboard.get_todays_activities(user))
     |> render("index.html")
+  end
+
+  def parse_date(nil) do
+    Timex.today
+  end
+
+  def parse_date(date) do
+    case Timex.parse(date, "{YYYY}-{0M}-{0D}") do
+      {:ok, date} -> date
+      {:error, _} -> Timex.today
+    end
   end
 end
--- lib/squeeze_web/views/calendar_view.ex
+++ lib/squeeze_web/views/calendar_view.ex
@@ -1,38 +1,24 @@
 defmodule SqueezeWeb.CalendarView do
   use SqueezeWeb, :view
 
-  def today do
-    Timex.today()
-  end
-
-  def days_in_month do
-    today()
-    |> Date.days_in_month()
-  end
-
-  def weeks_in_month do
-    days_in_month() / 7
-  end
-
-  def first_of_month do
-    today = today()
-    {:ok, date} = Date.new(today.year, today.month, 1)
+  def first_of_month(base) do
+    {:ok, date} = Date.new(base.year, base.month, 1)
     date
   end
 
-  def last_day_of_month do
-    today = today()
-    {:ok, date} = Date.new(today.year, today.month, Date.days_in_month(today))
+  def last_day_of_month(base) do
+    days = Date.days_in_month(base)
+    {:ok, date} = Date.new(base.year, base.month, days)
     date
   end
 
-  def dates do
-    Date.range(first_date(), last_date())
+  def dates(base) do
+    Date.range(first_date(base), last_date(base))
   end
 
   # 7 is Sunday in elxir
-  def first_date() do
-    date = first_of_month()
+  def first_date(base) do
+    date = first_of_month(base)
     day_of_week = Date.day_of_week(date)
     if day_of_week == 7 do
       date
@@ -42,8 +28,8 @@ defmodule SqueezeWeb.CalendarView do
   end
 
   # Last day is 6 Saturday
-  def last_date() do
-    date = last_day_of_month()
+  def last_date(base) do
+    date = last_day_of_month(base)
     day_of_week = Date.day_of_week(date)
     if day_of_week == 7 do
       Date.add(date, 6)
