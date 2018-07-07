054c2df8624cd3e4544f5124a405c4164b3670ce
Evan Dancer <evancdancer@gmail.com>
2018-07-07 16:24:54 -0700

    Use timex to format goal duration

--- lib/squeeze_web/views/sidebar_view.ex
+++ lib/squeeze_web/views/sidebar_view.ex
@@ -1,25 +1,15 @@
 defmodule SqueezeWeb.SidebarView do
   use SqueezeWeb, :view
 
+  alias Timex.Duration
+
   def full_name(user) do
     "#{user.first_name} #{user.last_name}"
   end
 
   def goal_time(goal) do
-    duration = goal.duration
-    hours = div(duration, 3600)
-    minutes = div((duration - hours * 3600), 60)
-    seconds = rem(duration, 60)
-    "#{hours}:#{minutes}:#{seconds}"
-  end
-
-  def goal_pace(goal) do
-    # min / mile
-    miles = goal.distance / 1609
-    pace = round(goal.duration / miles)
-    minutes = div(pace, 60)
-    seconds = rem(pace, 60)
-    secs = if seconds < 10, do: "0#{seconds}", else: seconds
-    "#{minutes}:#{secs}/mi"
+    Duration.from_seconds(goal.duration)
+    |> Duration.to_time!()
+    |> Timex.format!("%H:%M:%S", :strftime)
   end
 end
