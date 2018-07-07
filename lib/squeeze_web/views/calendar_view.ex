929db3739c2309fc458360a97a3286748b2c6a2f
Evan Dancer <evancdancer@gmail.com>
2018-07-07 10:53:57 -0700

    Add month and today buttons to calendar

--- lib/squeeze_web/views/calendar_view.ex
+++ lib/squeeze_web/views/calendar_view.ex
@@ -1,6 +1,16 @@
 defmodule SqueezeWeb.CalendarView do
   use SqueezeWeb, :view
 
+  def previous(base) do
+    {:ok, date} = subtract_month(base)
+    format_date(date)
+  end
+
+  def next(base) do
+    {:ok, date} = add_month(base)
+    format_date(date)
+  end
+
   def first_of_month(base) do
     {:ok, date} = Date.new(base.year, base.month, 1)
     date
@@ -36,5 +46,23 @@ defmodule SqueezeWeb.CalendarView do
     else
       Date.add(date, 6 - day_of_week)
     end
+  end
+
+  def add_month(date) do
+    case date.month do
+      12 -> Date.new(date.year + 1, 1, 1)
+      x -> Date.new(date.year, x + 1, 1)
+    end
+  end
+
+  def subtract_month(date) do
+    case date.month do
+      1 -> Date.new(date.year - 1, 12, 1)
+      x -> Date.new(date.year, x - 1, 1)
+    end
+  end
+
+  def format_date(date) do
+    Timex.format!(date, "{YYYY}-{0M}-{0D}")
   end
 end
