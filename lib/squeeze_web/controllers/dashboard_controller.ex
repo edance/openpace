ace0d06f155e34dbc1ec8728c39ed4f5b2e5e465
Evan Dancer <evancdancer@gmail.com>
2018-07-07 18:15:19 -0700

    Fix warning in dashboard controller

--- lib/squeeze_web/controllers/dashboard_controller.ex
+++ lib/squeeze_web/controllers/dashboard_controller.ex
@@ -1,8 +1,6 @@
 defmodule SqueezeWeb.DashboardController do
   use SqueezeWeb, :controller
 
-  alias Squeeze.Dashboard
-
   def index(conn, _params) do
     conn
     |> redirect(to: event_path(conn, :index))
