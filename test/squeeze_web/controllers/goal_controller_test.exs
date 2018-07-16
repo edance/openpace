ae77a0658d492b2f0fe85ac2876a4a2146ae8736
Evan Dancer <evancdancer@gmail.com>
2018-07-15 18:23:15 -0700

    Update goal controller specs

--- test/squeeze_web/controllers/goal_controller_test.exs
+++ test/squeeze_web/controllers/goal_controller_test.exs
@@ -1,16 +1,8 @@
 defmodule SqueezeWeb.GoalControllerTest do
   use SqueezeWeb.ConnCase
 
-  alias Squeeze.Dashboard
-
-  @create_attrs %{date: ~D[2010-04-17], distance: 120.5, duration: 42, name: "some name"}
-  @update_attrs %{date: ~D[2011-05-18], distance: 456.7, duration: 43, name: "some updated name"}
+  @valid_attrs %{date: ~D[2010-04-17], distance: 120.5, duration: 42, name: "some name"}
   @invalid_attrs %{date: nil, distance: nil, duration: nil, name: nil}
-
-  def fixture(:goal) do
-    {:ok, goal} = Dashboard.create_goal(@create_attrs)
-    goal
-  end
 
   @tag :as_user
   test "lists all goals on index", %{conn: conn} do
@@ -22,70 +14,45 @@ defmodule SqueezeWeb.GoalControllerTest do
     assert String.contains?(conn.resp_body, goal.name)
   end
 
-  describe "new goal" do
-    test "renders form", %{conn: conn} do
-      conn = get conn, goal_path(conn, :new)
-      assert html_response(conn, 200) =~ "New Goal"
-    end
+  @tag :as_user
+  test "renders form to create new goal", %{conn: conn} do
+    conn = get(conn, goal_path(conn, :new))
+    assert html_response(conn, 200) =~ ~r/New/
   end
 
-  describe "create goal" do
-    test "redirects to show when data is valid", %{conn: conn} do
-      conn = post conn, goal_path(conn, :create), goal: @create_attrs
+  @tag :as_user
+  test "creates goal and redirects", %{conn: conn} do
+    conn = post(conn, goal_path(conn, :create), goal: @valid_attrs)
 
-      assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == goal_path(conn, :show, id)
-
-      conn = get conn, goal_path(conn, :show, id)
-      assert html_response(conn, 200) =~ "Show Goal"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post conn, goal_path(conn, :create), goal: @invalid_attrs
-      assert html_response(conn, 200) =~ "New Goal"
-    end
+    assert %{id: id} = redirected_params(conn)
+    assert redirected_to(conn) == goal_path(conn, :show, id)
   end
 
-  describe "edit goal" do
-    setup [:create_goal]
+  @tag :as_user
+  test "does not create with invalid attributes", %{conn: conn} do
+    conn = post(conn, goal_path(conn, :create), goal: @invalid_attrs)
 
-    test "renders form for editing chosen goal", %{conn: conn, goal: goal} do
-      conn = get conn, goal_path(conn, :edit, goal)
-      assert html_response(conn, 200) =~ "Edit Goal"
-    end
+    assert html_response(conn, 200) =~ ~r/New/
   end
 
-  describe "update goal" do
-    setup [:create_goal]
-
-    test "redirects when data is valid", %{conn: conn, goal: goal} do
-      conn = put conn, goal_path(conn, :update, goal), goal: @update_attrs
-      assert redirected_to(conn) == goal_path(conn, :show, goal)
-
-      conn = get conn, goal_path(conn, :show, goal)
-      assert html_response(conn, 200) =~ "some updated name"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, goal: goal} do
-      conn = put conn, goal_path(conn, :update, goal), goal: @invalid_attrs
-      assert html_response(conn, 200) =~ "Edit Goal"
-    end
+  @tag :as_user
+  test "renders form for editing chosen goal", %{conn: conn} do
+    goal = insert(:goal, user: conn.assigns.current_user)
+    conn = get conn, goal_path(conn, :edit, goal)
+    assert html_response(conn, 200) =~ "Edit Goal"
   end
 
-  describe "delete goal" do
-    setup [:create_goal]
-
-    test "deletes chosen goal", %{conn: conn, goal: goal} do
-      conn = delete conn, goal_path(conn, :delete, goal)
-      assert redirected_to(conn) == goal_path(conn, :index)
-      assert_error_sent 404, fn ->
-        get conn, goal_path(conn, :show, goal)
-      end
-    end
+  @tag :as_user
+  test "updates goal and redirects", %{conn: conn} do
+    goal = insert(:goal, user: conn.assigns.current_user)
+    conn = put conn, goal_path(conn, :update, goal), goal: @valid_attrs
+    assert redirected_to(conn) == goal_path(conn, :show, goal)
   end
 
-  defp create_goal(_) do
-    goal = fixture(:goal)
-    {:ok, goal: goal}
+  @tag :as_user
+  test "deletes chosen goal on delete", %{conn: conn} do
+    goal = insert(:goal, user: conn.assigns.current_user)
+    conn = delete conn, goal_path(conn, :delete, goal)
+    assert redirected_to(conn) == goal_path(conn, :index)
   end
 end
