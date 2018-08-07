fecdc70b44e6ef8ba6246884570bff0d04df3c83
Evan Dancer <evancdancer@gmail.com>
2018-08-06 21:16:28 -0700

    Add distances for races

--- lib/squeeze_web/views/page_view.ex
+++ lib/squeeze_web/views/page_view.ex
@@ -1,3 +1,12 @@
 defmodule SqueezeWeb.PageView do
   use SqueezeWeb, :view
+
+  def distances() do
+    [
+      "5k": 5000,
+      "10k": 10000,
+      "Half Marathon": 21097,
+      "Marathon": 42195
+    ]
+  end
 end
