f23c1a17e37c3dc69b60fc62aebb453a1c9a794c
Evan Dancer <evancdancer@gmail.com>
2018-08-12 17:55:37 -0700

    Add style to duration dropdown

--- lib/squeeze_web/views/input_helpers.ex
+++ lib/squeeze_web/views/input_helpers.ex
@@ -3,26 +3,39 @@ defmodule SqueezeWeb.InputHelpers do
 
   import Phoenix.HTML
 
-  def duration_select(form, field) do
+  require IEx
+
+  map =
+    &Enum.map(&1, fn i ->
+      pre = if i < 10, do: "0"
+      {"#{pre}#{i}", i}
+    end)
+
+  @minsec map.(0..59)
+
+  def duration_select(form, field, opts \\ []) do
     id = input_id(form, field)
     name = input_name(form, field)
     value = input_value(form, field) || %{}
 
-    content_tag :div do
+    content_tag(:div, class: "duration-select")do
       [
-        select_tag(form, field, :hours, value),
+        select_tag(form, field, :hours, value, opts),
         ":",
-        select_tag(form, field, :minutes, value),
+        select_tag(form, field, :minutes, value, opts),
         ":",
-        select_tag(form, field, :seconds, value)
+        select_tag(form, field, :seconds, value, opts)
       ]
     end
   end
 
-  defp select_tag(form, parent, field, value) do
-    id = "#{input_id(form, parent)}_#{field}"
-    name = "#{input_name(form, parent)}[#{field}]"
-    value = Map.get(value, field)
-    select(form, parent, 0..59, prompt: field, value: value, id: id, name: name)
+  defp select_tag(form, parent, field, value, opts) do
+    opts = opts
+    |> Keyword.put(:id, "#{input_id(form, parent)}_#{field}")
+    |> Keyword.put(:name, "#{input_name(form, parent)}[#{field}]")
+    |> Keyword.put(:value, Map.get(value, field))
+    |> Keyword.put(:prompt, field)
+
+    select(form, parent, @minsec, opts)
   end
 end
