041ab20f07f9faba3251bcab46da9800f52d1064
Evan Dancer <evancdancer@gmail.com>
2018-08-18 20:35:47 -0700

    Add custom radio button

--- lib/squeeze_web/views/input_helpers.ex
+++ lib/squeeze_web/views/input_helpers.ex
@@ -11,12 +11,22 @@ defmodule SqueezeWeb.InputHelpers do
 
   @minsec map.(0..59)
 
+  def pill_button(form, field, label, value, opts \\ []) do
+    content_tag(:label, class: "pill-button") do
+      [
+        label,
+        radio_button(form, field, value, opts),
+        raw("<span class='checkmark'></span>")
+      ]
+    end
+  end
+
   def duration_select(form, field, opts \\ []) do
     id = input_id(form, field)
     name = input_name(form, field)
     value = input_value(form, field) || %{}
 
-    content_tag(:div, class: "duration-select")do
+    content_tag(:div, class: "duration-select") do
       [
         select_tag(form, field, :hours, value, opts),
         select_tag(form, field, :minutes, value, opts),
