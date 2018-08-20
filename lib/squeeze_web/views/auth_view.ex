a5b66575345b6f746e46a23a8365eeb63a6fad34
Evan Dancer <evancdancer@gmail.com>
2018-08-19 18:53:02 -0700

    Add auth controller and view tests

--- lib/squeeze_web/views/auth_view.ex
+++ lib/squeeze_web/views/auth_view.ex
@@ -1,3 +1,7 @@
 defmodule SqueezeWeb.AuthView do
   use SqueezeWeb, :view
+
+  def title(_page, _assigns) do
+    "Sign into your Account"
+  end
 end
---
+++ test/squeeze_web/controllers/auth_controller_test.exs
@@ -0,0 +1,19 @@
+defmodule SqueezeWeb.AuthControllerTest do
+  use SqueezeWeb.ConnCase
+
+  test "renders login page", %{conn: conn} do
+    conn = get(conn, auth_path(conn, :login))
+    assert html_response(conn, 200) =~ ~r/Welcome/
+  end
+
+  test "request with strava redirects to strava url", %{conn: conn} do
+    conn = get(conn, auth_path(conn, :request, "strava"))
+    assert redirected_to(conn) =~ ~r/https:\/\/www.strava.com/
+  end
+
+  test "logs out user on delete", %{conn: conn} do
+    conn = delete conn, auth_path(conn, :delete)
+    user = Squeeze.Guardian.Plug.current_resource(conn)
+    assert user == nil
+  end
+end
