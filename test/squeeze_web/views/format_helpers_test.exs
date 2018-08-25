3f4910852b18aa080aa3053a18812642c470405a
Evan Dancer <evancdancer@gmail.com>
2018-08-24 21:40:52 -0700

    Add specs for helpers

--- test/squeeze_web/views/format_helpers_test.exs
+++ test/squeeze_web/views/format_helpers_test.exs
@@ -24,4 +24,35 @@ defmodule SqueezeWeb.FormatHelpersTest do
       assert SqueezeWeb.FormatHelpers.format_duration(duration) == "9:00"
     end
   end
+
+  describe "#relative_time" do
+    test "returns Timex.format with relative" do
+      now = Timex.now
+      relative_time = SqueezeWeb.FormatHelpers.relative_time(now)
+      assert relative_time == Timex.format!(now, "{relative}", :relative)
+    end
+  end
+
+  describe "#format_pace" do
+    test "with a valid distance" do
+      distance = 2 * 1609 # 2 miles in meters
+      duration = %{total: 14 * 60} # 14 minutes in seconds
+      pace_str = SqueezeWeb.FormatHelpers.format_pace(duration, distance)
+      assert pace_str == "7:00 min/mile"
+    end
+
+    test "with an invalid distance > 0" do
+      distance = 1 # 1 meter
+      duration = %{total: 14 * 60} # 14 minutes in seconds
+      pace_str = SqueezeWeb.FormatHelpers.format_pace(duration, distance)
+      assert pace_str == ""
+    end
+
+    test "with a distance of 0" do
+      distance = 0 # 1 meter
+      duration = %{total: 30 * 60} # 30 minutes in seconds
+      pace_str = SqueezeWeb.FormatHelpers.format_pace(duration, distance)
+      assert pace_str == ""
+    end
+  end
 end
