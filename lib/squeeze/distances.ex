0ab006ec7d3988db2213d546d555c54e372da8aa
Evan Dancer <evancdancer@gmail.com>
2018-09-03 11:43:55 -0700

    Add distance parser

--- lib/squeeze/distances.ex
+++ lib/squeeze/distances.ex
@@ -10,12 +10,39 @@ defmodule Squeeze.Distances do
     %{name: "Marathon", distance: 42_195},
   ]
 
-  def distances do
-    @distances
-  end
+  @mile_in_meters 1609 # 1 mile == 1609 meters
+
+  def mile_in_meters, do: @mile_in_meters
+
+  def distances, do: @distances
 
   def from_meters(meters) do
     @distances
     |> Enum.find(fn(x) -> x.distance == meters end)
+  end
+
+  def parse({distance, "mi"}), do: {:ok, distance * @mile_in_meters}
+  def parse({distance, "mile"}), do: {:ok, distance * @mile_in_meters}
+  def parse({distance, "miles"}), do: {:ok, distance * @mile_in_meters}
+  def parse({distance, "m"}), do: {:ok, distance}
+  def parse({distance, "k"}), do: {:ok, distance * 1000}
+  def parse({distance, "km"}), do: {:ok, distance * 1000}
+  def parse({distance, ""}), do: {:ok, distance * @mile_in_meters}
+  def parse({multiplier, "x" <> rest}) do
+    case parse(rest) do
+      {:ok, distance} -> {:ok, multiplier * distance}
+      {:error} -> {:error}
+    end
+  end
+  def parse(str) when is_binary(str) do
+    str
+    |> remove_whitespace()
+    |> Integer.parse()
+    |> parse()
+  end
+  def parse(_), do: {:error}
+
+  def remove_whitespace(str) do
+    str |> String.replace(" ", "")
   end
 end
---
+++ test/squeeze/distances_test.exs
@@ -0,0 +1,58 @@
+defmodule Squeeze.DistancesTest do
+  use Squeeze.DataCase
+
+  @moduledoc false
+
+  alias Squeeze.Distances
+
+  test "mile_in_meters" do
+    assert Distances.mile_in_meters == 1609
+  end
+
+  describe "#parse" do
+    test "default number to miles" do
+      assert {:ok, distance} = Distances.parse("3")
+      assert distance == Distances.mile_in_meters * 3
+    end
+
+    test "parse for mi" do
+      assert {:ok, distance} = Distances.parse("12mi")
+      assert distance == 1609 * 12
+    end
+
+    test "parse for miles" do
+      assert {:ok, distance} = Distances.parse("12 miles")
+      assert distance == 1609 * 12
+    end
+
+    test "parse for km" do
+      assert {:ok, distance} = Distances.parse("5 km")
+      assert distance == 5000
+    end
+
+    test "parse for k" do
+      assert {:ok, distance} = Distances.parse("5k")
+      assert distance == 5000
+    end
+
+    test "parse for m" do
+      assert {:ok, distance} = Distances.parse("1600m")
+      assert distance == 1600
+    end
+
+    test "multiplication with mile repeats" do
+      assert {:ok, distance} = Distances.parse("4 x 1mi")
+      assert distance == 1609 * 4
+    end
+
+    test "multiplication with km repeats" do
+      assert {:ok, distance} = Distances.parse("4 x 1km")
+      assert distance == 1000 * 4
+    end
+
+    test "multiplication with m repeats" do
+      assert {:ok, distance} = Distances.parse("4x1600m")
+      assert distance == 1600 * 4
+    end
+  end
+end
