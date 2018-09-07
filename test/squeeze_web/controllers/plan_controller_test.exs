50cb7f77af2d469da005d26323ef79d193130eeb
Evan Dancer <evancdancer@gmail.com>
2018-09-06 20:28:15 -0700

    Add some basic controller specs

---
+++ test/squeeze_web/controllers/plan_controller_test.exs
@@ -0,0 +1,30 @@
+defmodule SqueezeWeb.PlanControllerTest do
+  use SqueezeWeb.ConnCase
+
+  describe "#step" do
+    test "valid step name", %{conn: conn} do
+      conn = get(conn, "/dashboard/plan/weeks")
+      assert html_response(conn, 200) =~ "weeks"
+    end
+
+    test "invalid step name", %{conn: conn} do
+      conn = get(conn, "/dashboard/plan/abcd")
+      assert html_response(conn, 404) =~ "not found"
+    end
+  end
+
+  describe "#update" do
+    test "weeks updates session and redirects", %{conn: conn} do
+      conn = post(conn, "/dashboard/plan/weeks", weeks: "18")
+      assert get_session(conn, :weeks) == 18
+      assert redirected_to(conn) == plan_path(conn, :step, "start")
+    end
+
+    test "start updates session and redirects", %{conn: conn} do
+      date = "2018-01-01"
+      conn = post(conn, "/dashboard/plan/start", start_at: date)
+      assert get_session(conn, :start_at) == Timex.parse!(date, "{YYYY}-{0M}-{0D}")
+      assert redirected_to(conn) == plan_path(conn, :new, 1)
+    end
+  end
+end
