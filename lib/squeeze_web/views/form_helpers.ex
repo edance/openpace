df74e4dc80368277d8496bfcf9eabaf288a3ff19
Evan Dancer <evancdancer@gmail.com>
2018-11-09 07:53:50 -0800

    Update duration select style

--- lib/squeeze_web/views/form_helpers.ex
+++ lib/squeeze_web/views/form_helpers.ex
@@ -56,7 +56,7 @@ defmodule SqueezeWeb.FormHelpers do
   def duration_select(form, field, opts \\ []) do
     value = input_value(form, field) || %{}
 
-    content_tag(:div, class: "duration-select") do
+    content_tag(:div, class: "form-row") do
       [
         select_tag(form, field, :hours, value, opts),
         select_tag(form, field, :minutes, value, opts),
@@ -72,6 +72,8 @@ defmodule SqueezeWeb.FormHelpers do
     |> Keyword.put(:value, Map.get(value, field))
     |> Keyword.put(:prompt, field)
 
-    select(form, parent, @minsec, opts)
+    content_tag(:div, class: "col") do
+      select(form, parent, @minsec, opts)
+    end
   end
 end
