b83d4dace4304e3fe2f62638b181c31f5e0f25ef
Evan Dancer <evancdancer@gmail.com>
2018-11-09 20:41:58 -0800

    Move async form javascript to separate file

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,8 +13,6 @@
 // to also remove its path from "config.paths.watched".
 import 'phoenix_html';
 import 'bootstrap';
-import 'whatwg-fetch';
-import { u } from 'umbrellajs';
 
 // Import local files
 //
@@ -25,30 +23,3 @@ import './components/base';
 // Turbolinks
 import Turbolinks from 'turbolinks';
 Turbolinks.start();
-
-// Found in changelog
-// https://github.com/thechangelog/changelog.com/blob/master/assets/app/app.js#L121
-// submit forms with Turbolinks
-u(document).on('submit', 'form', function(event) {
-  event.preventDefault();
-
-  const form = u(this);
-  const action = form.attr('action');
-  const method = form.attr('method');
-  const referrer = location.href;
-
-  if (method == 'get') {
-    return Turbolinks.visit(`${action}?${form.serialize()}`);
-  }
-
-  const options = {method: method, body: new FormData(form.first()), headers: {'Turbolinks-Referrer': referrer}};
-  const andThen = ([resp, body]) => {
-    const snapshot = Turbolinks.Snapshot.wrap(body);
-    Turbolinks.controller.cache.put(resp.url, snapshot);
-    Turbolinks.visit(resp.url, {action: 'restore'});
-  };
-
-  fetch(action, options)
-    .then(resp => Promise.all([resp, resp.text()]))
-    .then(andThen);
-});
