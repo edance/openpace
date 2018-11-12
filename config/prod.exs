db311222101f629bdf33153d527b9b57f7b229c5
Evan Dancer <evancdancer@gmail.com>
2018-11-11 18:15:29 -0800

    Update config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,12 +10,19 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :squeeze, SqueezeWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: URI.parse(System.get_env("HOST_URL")).host, port: System.get_env("PORT")],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :squeeze, Squeeze.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -68,4 +75,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
