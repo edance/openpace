b274efb3aec6149e754d184db2af7ac3fb3290e0
Evan Dancer <evancdancer@gmail.com>
2018-11-15 20:36:19 -0800

    Remove custom type of duration

--- lib/squeeze_web/views/form_helpers.ex
+++ lib/squeeze_web/views/form_helpers.ex
@@ -8,14 +8,6 @@ defmodule SqueezeWeb.FormHelpers do
 
   import Phoenix.HTML
 
-  map =
-    &Enum.map(&1, fn i ->
-      pre = if i < 10, do: "0"
-      {"#{pre}#{i}", i}
-    end)
-
-  @minsec map.(0..59)
-
   def autocomplete(form, field, options, opts \\ []) do
     list_name = "autocomplete-#{:rand.uniform(1000)}"
     option_html = options
@@ -49,30 +41,6 @@ defmodule SqueezeWeb.FormHelpers do
         label,
         radio_button(form, field, value, opts)
       ]
-    end
-  end
-
-  def duration_select(form, field, opts \\ []) do
-    value = input_value(form, field) || %{}
-
-    content_tag(:div, class: "form-row") do
-      [
-        select_tag(form, field, :hours, value, opts),
-        select_tag(form, field, :minutes, value, opts),
-        select_tag(form, field, :seconds, value, opts)
-      ]
-    end
-  end
-
-  defp select_tag(form, parent, field, value, opts) do
-    opts = opts
-    |> Keyword.put(:id, "#{input_id(form, parent)}_#{field}")
-    |> Keyword.put(:name, "#{input_name(form, parent)}[#{field}]")
-    |> Keyword.put(:value, Map.get(value, field))
-    |> Keyword.put(:prompt, field)
-
-    content_tag(:div, class: "col") do
-      select(form, parent, @minsec, opts)
     end
   end
 end
