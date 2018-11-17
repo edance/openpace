30d776fa436ea2a7f34009cef8b83370f99c9f85
Evan Dancer <evancdancer@gmail.com>
2018-11-16 20:25:46 -0800

    Update the style of the quiz steps

--- assets/js/components/date-picker.js
+++ assets/js/components/date-picker.js
@@ -1,7 +1,5 @@
-import { u } from 'umbrellajs';
 import flatpickr from 'flatpickr';
 
 document.addEventListener("turbolinks:load", function() {
-  u('.flatpickr-calendar').remove();
-  flatpickr('.date-picker', { inline: true, static: true });
+  flatpickr('.date-picker');
 });
