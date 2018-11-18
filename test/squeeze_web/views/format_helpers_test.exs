d5060cdd3e039fe8a8e59294ef9870d47ccc3ca7
Evan Dancer <evancdancer@gmail.com>
2018-11-18 14:58:02 -0800

    Fix duration helper specs

--- test/squeeze_web/views/format_helpers_test.exs
+++ test/squeeze_web/views/format_helpers_test.exs
@@ -6,23 +6,27 @@ defmodule SqueezeWeb.FormatHelpersTest do
   alias SqueezeWeb.FormatHelpers
 
   describe "#format_duration" do
+    test "with a nil duration" do
+      assert FormatHelpers.format_duration(nil) == nil
+    end
+
     test "with a full duration" do
-      duration = %{total: 3 * 3600 + 20 * 60}
+      duration = 3 * 3600 + 20 * 60
       assert FormatHelpers.format_duration(duration) == "3:20:00"
     end
 
     test "without an hour" do
-      duration = %{total: 20 * 60}
+      duration = 20 * 60
       assert FormatHelpers.format_duration(duration) == "20:00"
     end
 
     test "with a minute under 10 and an hour" do
-      duration = %{total: 3 * 3600 + 9 * 60}
+      duration = 3 * 3600 + 9 * 60
       assert FormatHelpers.format_duration(duration) == "3:09:00"
     end
 
     test "with a minute under 10 and no hour" do
-      duration = %{total: 9 * 60}
+      duration = 9 * 60
       assert FormatHelpers.format_duration(duration) == "9:00"
     end
   end
@@ -38,21 +42,21 @@ defmodule SqueezeWeb.FormatHelpersTest do
   describe "#format_pace" do
     test "with a valid distance" do
       distance = 2 * 1609 # 2 miles in meters
-      duration = %{total: 14 * 60} # 14 minutes in seconds
+      duration = 14 * 60 # 14 minutes in seconds
       pace_str = FormatHelpers.format_pace(duration, distance)
       assert pace_str == "7:00 min/mile"
     end
 
     test "with an invalid distance > 0" do
       distance = 1 # 1 meter
-      duration = %{total: 14 * 60} # 14 minutes in seconds
+      duration = 14 * 60 # 14 minutes in seconds
       pace_str = FormatHelpers.format_pace(duration, distance)
       assert pace_str == ""
     end
 
     test "with a distance of 0" do
       distance = 0 # 1 meter
-      duration = %{total: 30 * 60} # 30 minutes in seconds
+      duration = 30 * 60 # 30 minutes in seconds
       pace_str = FormatHelpers.format_pace(duration, distance)
       assert pace_str == ""
     end
