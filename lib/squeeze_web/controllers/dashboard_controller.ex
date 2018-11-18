9d84b2e436a971540af97f0af21c275b7fb82b2b
Evan Dancer <evancdancer@gmail.com>
2018-11-18 14:50:05 -0800

    Add task to dashboard index to sync with strava

--- lib/squeeze_web/controllers/dashboard_controller.ex
+++ lib/squeeze_web/controllers/dashboard_controller.ex
@@ -2,15 +2,22 @@ defmodule SqueezeWeb.DashboardController do
   use SqueezeWeb, :controller
 
   alias Squeeze.Dashboard
+  alias Squeeze.Sync
 
-  def index(conn, _params) do
-    user = conn.assigns.current_user
-    events = Dashboard.list_past_events(user)
-    activities = Dashboard.list_activities(conn.assigns.current_user)
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns.current_user]
+    apply(__MODULE__, action_name(conn), args)
+  end
+
+  def index(conn, _params, current_user) do
+    Task.start(fn -> Sync.load_activities(current_user) end)
+
+    events = Dashboard.list_past_events(current_user)
+    activities = Dashboard.list_activities(current_user)
     render(conn, "index.html", activities: activities, events: events)
   end
 
-  def example(conn, _) do
+  def example(conn, _params, _current_user) do
     render(conn, "example.html")
   end
 end
