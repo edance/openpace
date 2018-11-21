7dbd8864de7c9337595ae1b913ac2dbdf4a92f4b
Evan Dancer <evancdancer@gmail.com>
2018-11-21 10:18:56 -0800

    Finalize goal card

--- lib/squeeze_web/views/dashboard_view.ex
+++ lib/squeeze_web/views/dashboard_view.ex
@@ -2,6 +2,7 @@ defmodule SqueezeWeb.DashboardView do
   use SqueezeWeb, :view
 
   alias Squeeze.Distances
+  alias Squeeze.Accounts.User
 
   require IEx
 
@@ -13,10 +14,17 @@ defmodule SqueezeWeb.DashboardView do
     "#{user.first_name} #{user.last_name}"
   end
 
-  def goal_card_title(user) do
+  def race_name(user) do
     distance = user.user_prefs.distance
     %{name: name} = Distances.from_meters(distance)
-    "#{name} Goal"
+    name
+  end
+
+  def improvement_amount(%User{user_prefs: %{personal_record: nil}}), do: nil
+  def improvement_amount(%User{user_prefs: prefs}) do
+    personal_record = prefs.personal_record
+    percent = abs(personal_record - prefs.duration) / personal_record * 100
+    "#{Decimal.round(Decimal.new(percent), 1)}%"
   end
 
   def format_goal(user) do
