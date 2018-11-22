70c3e19c8c7b5391feac5ec9d3235029e703c81e
Evan Dancer <evancdancer@gmail.com>
2018-11-22 10:25:51 -0800

    Update to phoenix 1.3.4

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Squeeze.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, "~> 1.3.4"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
@@ -45,6 +45,7 @@ defmodule Squeeze.Mixfile do
       {:turbolinks, "~> 0.3.4"},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 1.0"},
       {:strava, git: "https://github.com/slashdotdash/strava.git", branch: "master"},
       {:timex, "~> 3.3"},
       {:guardian, "~> 1.0"},
