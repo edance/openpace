ced7511ec0c8b30e5d9352a2bd60d416a0032494
Evan Dancer <evancdancer@gmail.com>
2018-11-23 07:23:49 -0800

    Add update specs for wizard controller

--- test/squeeze_web/controllers/wizard_controller_test.exs
+++ test/squeeze_web/controllers/wizard_controller_test.exs
@@ -2,7 +2,7 @@ defmodule SqueezeWeb.WizardControllerTest do
   use SqueezeWeb.ConnCase
 
   test "index redirects to the first step", %{conn: conn} do
-    conn = get(conn, "/quiz")
+    conn = get(conn, wizard_path(conn, :index))
     assert redirected_to(conn) == page_path(conn, :index)
   end
 
@@ -26,18 +26,18 @@ defmodule SqueezeWeb.WizardControllerTest do
     end
   end
 
-  # describe "#update" do
-  #   test "weeks updates session and redirects", %{conn: conn} do
-  #     conn = post(conn, "/quiz/duration", weeks: 12345)
-  #     assert get_session(conn, :weeks) == 18
-  #     assert redirected_to(conn) == plan_path(conn, :step, "start")
-  #   end
+  describe "#update" do
+    test "redirects when data is valid", %{conn: conn} do
+      step = "distance"
+      next_step = "race-date"
+      conn = put conn, wizard_path(conn, :update, step), user_prefs: %{distance: 1}
+      assert redirected_to(conn) == wizard_path(conn, :step, next_step)
+    end
 
-  #   test "start updates session and redirects", %{conn: conn} do
-  #     date = "2018-01-01"
-  #     conn = post(conn, "/dashboard/plan/start", start_at: date)
-  #     assert get_session(conn, :start_at) == Timex.parse!(date, "{YYYY}-{0M}-{0D}")
-  #     assert redirected_to(conn) == plan_path(conn, :new, 1)
-  #   end
-  # end
+    test "renders errors when data is invalid", %{conn: conn} do
+      step = "distance"
+      conn = put conn, wizard_path(conn, :update, step), user_prefs: %{distance: nil}
+      assert html_response(conn, 200) =~ "Choose a distance"
+    end
+  end
 end
