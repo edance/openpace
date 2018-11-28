ac2f8d61abb210d1d53909823b5750380b51e830
Evan Dancer <evancdancer@gmail.com>
2018-11-28 10:33:40 -0800

    Use series for all dates in range

--- lib/squeeze/stats.ex
+++ lib/squeeze/stats.ex
@@ -10,17 +10,27 @@ defmodule Squeeze.Stats do
 
   def distance_by_month(%User{} = user) do
     query = from a in Activity,
-      where: [user_id: ^user.id],
-      group_by: fragment("date"),
-      select: [fragment("date_trunc('month', ?) as date", a.start_at), sum(a.distance)]
+      right_join: months in fragment("select generate_series(date_trunc('month', now()) - '1 year'::interval, date_trunc('month', now()), '1 month'::interval) as month"),
+      on: a.user_id == ^user.id and months.month == fragment("date_trunc('month', ?)", a.start_at),
+      group_by: months.month,
+      order_by: months.month,
+      select: %{
+        day: months.month,
+        sum: sum(a.distance)
+      }
     Repo.all(query)
   end
 
   def distance_by_day(%User{} = user) do
     query = from a in Activity,
-      where: [user_id: ^user.id],
-      group_by: fragment("date"),
-      select: [fragment("date_trunc('day', ?) as date", a.start_at), sum(a.distance)]
+      right_join: days in fragment("select generate_series(date_trunc('day', now()) - '1 month'::interval, date_trunc('day', now()), '1 day'::interval) as day"),
+      on: a.user_id == ^user.id and days.day == fragment("date_trunc('day', ?)", a.start_at),
+      group_by: days.day,
+      order_by: days.day,
+      select: %{
+        day: days.day,
+        sum: sum(a.distance)
+      }
     Repo.all(query)
   end
 end
