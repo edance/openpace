cdc5ceca72d05230ec16377f1cbbfda52c441474
Evan Dancer <evancdancer@gmail.com>
2018-12-17 20:55:37 -0800

    Add profile#update to save preferences

--- lib/squeeze_web/controllers/profile_controller.ex
+++ lib/squeeze_web/controllers/profile_controller.ex
@@ -1,7 +1,25 @@
 defmodule SqueezeWeb.ProfileController  do
   use SqueezeWeb, :controller
 
-  def show(conn, _params) do
-    render(conn, "show.html")
+  alias Squeeze.Accounts
+
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns.current_user]
+    apply(__MODULE__, action_name(conn), args)
+  end
+
+  def edit(conn, _params, current_user) do
+    changeset = Accounts.change_user(current_user)
+    render(conn, "edit.html", changeset: changeset)
+  end
+
+  def update(conn, %{"user" => user_params}, current_user) do
+    case Accounts.update_user(current_user, user_params) do
+      {:ok, _} ->
+        conn
+        |> redirect(to: dashboard_path(conn, :index))
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", changeset: changeset)
+    end
   end
 end
--- lib/squeeze_web/views/profile_view.ex
+++ lib/squeeze_web/views/profile_view.ex
@@ -2,6 +2,7 @@ defmodule SqueezeWeb.ProfileView do
   use SqueezeWeb, :view
 
   alias Squeeze.Accounts.User
+  alias Squeeze.Distances
 
   def title(_page, _assigns) do
     "Profile"
@@ -11,5 +12,10 @@ defmodule SqueezeWeb.ProfileView do
 
   def hometown(%User{city: city, state: state}) do
     "#{city}, #{state}"
+  end
+
+  def distances do
+    Distances.distances
+    |> Enum.map(fn(x) -> {x.name, x.distance} end)
   end
 end
