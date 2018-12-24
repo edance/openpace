e3617e5eb83c8c1d771ac7489221cb5f2de13a08
Evan Dancer <evancdancer@gmail.com>
2018-12-23 19:26:16 -0800

    Remove unused dashboard methods

--- lib/squeeze/dashboard/dashboard.ex
+++ lib/squeeze/dashboard/dashboard.ex
@@ -10,22 +10,6 @@ defmodule Squeeze.Dashboard do
   alias Squeeze.Repo
 
   @doc """
-  Returns the list of activities by user.
-
-  ## Examples
-
-      iex> list_activities(user)
-      [%Activity{}, ...]
-
-  """
-  def list_activities(user) do
-    Activity
-    |> where([a], a.user_id == ^user.id)
-    |> Repo.all
-    |> Repo.preload(:user)
-  end
-
-  @doc """
   Returns the list of activities by user in a date range.
 
   ## Examples
@@ -100,40 +84,6 @@ defmodule Squeeze.Dashboard do
   end
 
   @doc """
-  Updates a activity.
-
-  ## Examples
-
-  iex> update_activity(activity, %{field: new_value})
-  {:ok, %Activity{}}
-
-  iex> update_activity(activity, %{field: bad_value})
-  {:error, %Ecto.Changeset{}}
-
-  """
-  def update_activity(%Activity{} = activity, attrs) do
-    activity
-    |> Activity.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Activity.
-
-  ## Examples
-
-  iex> delete_activity(activity)
-  {:ok, %Activity{}}
-
-  iex> delete_activity(activity)
-  {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_activity(%Activity{} = activity) do
-    Repo.delete(activity)
-  end
-
-  @doc """
   Returns an `%Ecto.Changeset{}` for tracking activity changes.
 
   ## Examples
@@ -165,108 +115,5 @@ defmodule Squeeze.Dashboard do
     |> order_by([a], a.date)
     |> Repo.all
     |> Repo.preload(:user)
-  end
-
-  @doc """
-  Returns list of events by user in the past including today.
-
-  ## Examples
-
-      iex> list_past_events(user)
-      [%Event{}, ...]
-
-  """
-  def list_past_events(%User{} = user) do
-    Event
-    |> where([a], a.user_id == ^user.id)
-    |> where([a], a.date <= ^Date.utc_today())
-    |> Repo.all
-    |> Repo.preload(:user)
-  end
-
-  @doc """
-  Gets a single event.
-
-  Raises `Ecto.NoResultsError` if the Event does not exist.
-
-  ## Examples
-
-      iex> get_event!(123)
-      %Event{}
-
-      iex> get_event!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_event!(id) do
-    Event
-    |> Repo.get!(id)
-    |> Repo.preload(:user)
-  end
-
-  @doc """
-  Creates a event.
-
-  ## Examples
-
-      iex> create_event(%User{}, %{field: value})
-      {:ok, %Event{}}
-
-      iex> create_event(%User{}, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_event(%User{} = user, attrs \\ %{}) do
-    %Event{}
-    |> Event.changeset(attrs)
-    |> Changeset.put_change(:user_id, user.id)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a event.
-
-  ## Examples
-
-      iex> update_event(event, %{field: new_value})
-      {:ok, %Event{}}
-
-      iex> update_event(event, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_event(%Event{} = event, attrs) do
-    event
-    |> Event.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Event.
-
-  ## Examples
-
-      iex> delete_event(event)
-      {:ok, %Event{}}
-
-      iex> delete_event(event)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_event(%Event{} = event) do
-    Repo.delete(event)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking event changes.
-
-  ## Examples
-
-      iex> change_event(event)
-      %Ecto.Changeset{source: %Event{}}
-
-  """
-  def change_event(%Event{} = event) do
-    Event.changeset(event, %{})
   end
 end
--- test/squeeze/dashboard/dashboard_test.exs
+++ test/squeeze/dashboard/dashboard_test.exs
@@ -5,6 +5,43 @@ defmodule Squeeze.DashboardTest do
 
   import Squeeze.Factory
 
+  describe "#list_activities/2" do
+    test "includes only the users activities" do
+      activity1 = insert(:activity)
+      activity2 = insert(:activity, start_at: activity1.start_at)
+      activities = Dashboard.list_activities(activity1.user, nil)
+      assert activities |> Enum.map(&(&1.id)) |> Enum.member?(activity1.id)
+      refute activities |> Enum.map(&(&1.id)) |> Enum.member?(activity2.id)
+    end
+
+    test "includes only activities in the date range" do
+    end
+  end
+
+  describe "#recent_activities/1" do
+    test "includes only the users activities" do
+      activity1 = insert(:activity)
+      activity2 = insert(:activity, start_at: activity1.start_at)
+      activities = Dashboard.recent_activities(activity1.user)
+      assert activities |> Enum.map(&(&1.id)) |> Enum.member?(activity1.id)
+      refute activities |> Enum.map(&(&1.id)) |> Enum.member?(activity2.id)
+    end
+
+    test "orders by most recent activities first" do
+    end
+  end
+
+  describe "#get_activity!/1" do
+    test "returns the activity if found" do
+      activity = insert(:activity)
+      assert activity.id == Dashboard.get_activity!(activity.id).id
+    end
+
+    test "raises error if not found" do
+      assert_raise Ecto.NoResultsError, fn -> Dashboard.get_activity!("1234") end
+    end
+  end
+
   describe "events" do
     test "list_events/2 includes only the users events" do
       event1 = insert(:event)
@@ -23,14 +60,6 @@ defmodule Squeeze.DashboardTest do
       events = Dashboard.list_events(user, range)
       assert events |> Enum.map(&(&1.id)) |> Enum.member?(event1.id)
       refute events |> Enum.map(&(&1.id)) |> Enum.member?(event2.id)
-    end
-
-    test "list_past_events/1 returns only the users events in the past" do
-      old_event = insert(:event, date: Date.utc_today)
-      future_event = insert(:event, user: old_event.user)
-      events = Dashboard.list_past_events(future_event.user)
-      assert events |> Enum.map(&(&1.id)) |> Enum.member?(old_event.id)
-      refute events |> Enum.map(&(&1.id)) |> Enum.member?(future_event.id)
     end
   end
 end
