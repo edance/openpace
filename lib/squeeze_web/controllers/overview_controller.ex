cba2e301b49009311647cfb446ea58d7a700aee6
Evan Dancer <evancdancer@gmail.com>
2018-12-30 09:02:26 -0800

    Remove events from overview

--- lib/squeeze_web/controllers/overview_controller.ex
+++ lib/squeeze_web/controllers/overview_controller.ex
@@ -13,15 +13,8 @@ defmodule SqueezeWeb.OverviewController do
     activities = Dashboard.recent_activities(current_user)
     render(conn, "index.html",
       activities: activities,
-      events: events(current_user),
       week_dataset: Stats.dataset_for_week_chart(current_user),
       year_dataset: Stats.dataset_for_year_chart(current_user)
     )
-  end
-
-  defp events(current_user) do
-    today = Date.utc_today()
-    range = Date.range(today, Date.add(today, 1))
-    Dashboard.list_events(current_user, range)
   end
 end
--- test/squeeze/dashboard/dashboard_test.exs
+++ test/squeeze/dashboard/dashboard_test.exs
@@ -41,25 +41,4 @@ defmodule Squeeze.DashboardTest do
       assert_raise Ecto.NoResultsError, fn -> Dashboard.get_activity!("1234") end
     end
   end
-
-  describe "events" do
-    test "list_events/2 includes only the users events" do
-      event1 = insert(:event)
-      event2 = insert(:event, date: event1.date)
-      range = Date.range(event1.date, Date.add(event1.date, 1))
-      events = Dashboard.list_events(event1.user, range)
-      assert events |> Enum.map(&(&1.id)) |> Enum.member?(event1.id)
-      refute events |> Enum.map(&(&1.id)) |> Enum.member?(event2.id)
-    end
-
-    test "list_events/2 returns only the events in the range" do
-      user = insert(:user)
-      event1 = insert(:event, user: user)
-      event2 = insert(:event, user: user, date: Date.add(event1.date, 2))
-      range = Date.range(event1.date, Date.add(event1.date, 1))
-      events = Dashboard.list_events(user, range)
-      assert events |> Enum.map(&(&1.id)) |> Enum.member?(event1.id)
-      refute events |> Enum.map(&(&1.id)) |> Enum.member?(event2.id)
-    end
-  end
 end
