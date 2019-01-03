7e3efcb3ad9ebe887d56ad576bcd0f181dfec65e
Evan Dancer <evancdancer@gmail.com>
2019-01-03 07:30:55 -0800

    Filter activities by date range

--- lib/squeeze/time_helper.ex
+++ lib/squeeze/time_helper.ex
@@ -5,15 +5,25 @@ defmodule Squeeze.TimeHelper do
 
   alias Squeeze.Accounts.User
 
-  def today(%User{user_prefs: %{timezone: timezone}}) do
-    timezone
-    |> Timex.now()
-    |> Timex.to_date()
-  end
+  def today(%User{} = user), do: to_date(user)
 
   def to_date(%User{user_prefs: %{timezone: timezone}}, datetime \\ Timex.now) do
     datetime
     |> Timex.to_datetime(timezone)
     |> Timex.to_date()
+  end
+
+  def beginning_of_day(%User{user_prefs: %{timezone: timezone}}, date) do
+    date
+    |> Timex.to_datetime(timezone)
+    |> Timex.beginning_of_day()
+    |> Timex.to_datetime()
+  end
+
+  def end_of_day(%User{user_prefs: %{timezone: timezone}}, date) do
+    date
+    |> Timex.to_datetime(timezone)
+    |> Timex.end_of_day()
+    |> Timex.to_datetime()
   end
 end
