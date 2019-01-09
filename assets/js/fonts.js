30e70e2a0e2c843029558e9a532e0eda612bda88
Evan Dancer <evancdancer@gmail.com>
2019-01-08 20:05:41 -0800

    Add dashboard.js that loads mapbox and chart.js

---
+++ assets/js/fonts.js
@@ -0,0 +1,12 @@
+// Load fonts async
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  custom: {
+    families: ['simple-line-icons'],
+    urls: ['//cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css'],
+  },
+  google: {
+    families: ['Open Sans:300,400', 'Bree Serif'],
+  },
+});
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = (env, options) => ({
   },
   entry: {
     'app': './js/app.js',
+    'dashboard': './js/dashboard.js',
   },
   output: {
     filename: '[name].js',
