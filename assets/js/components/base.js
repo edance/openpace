9da5a9824692c1d4bda53b01e05151095ff3957b
Evan Dancer <evancdancer@gmail.com>
2019-01-27 14:55:27 -0800

    Rename credit-card-input to payment-form

--- assets/js/components/base.js
+++ assets/js/components/base.js
@@ -1,8 +1,8 @@
 import './autocomplete-input';
 import './calendar';
-import './credit-card-input';
 import './date-picker';
 import './duration-select';
 import './form';
 import './imperial-hidden-input';
+import './payment-form';
 import './timezone-hidden-input';
