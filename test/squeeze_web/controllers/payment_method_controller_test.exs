7b3fa57a1a7b9e7c70cac3d0ab46860db062abc6
Evan Dancer <evancdancer@gmail.com>
2019-01-27 21:00:24 -0800

    Add tests for payment_method_controller

--- test/squeeze_web/controllers/payment_method_controller_test.exs
+++ test/squeeze_web/controllers/payment_method_controller_test.exs
@@ -3,86 +3,53 @@ defmodule SqueezeWeb.PaymentMethodControllerTest do
 
   alias Squeeze.Billing
 
-  @create_attrs %{exp_month: 42, exp_year: 42, last4: "some last4", name: "some name", stripe_id: "some stripe_id"}
-  @update_attrs %{exp_month: 43, exp_year: 43, last4: "some updated last4", name: "some updated name", stripe_id: "some updated stripe_id"}
-  @invalid_attrs %{exp_month: nil, exp_year: nil, last4: nil, name: nil, stripe_id: nil}
-
-  def fixture(:payment_method) do
-    {:ok, payment_method} = Billing.create_payment_method(@create_attrs)
-    payment_method
-  end
-
-  describe "index" do
-    test "lists all payment_methods", %{conn: conn} do
-      conn = get conn, payment_method_path(conn, :index)
-      assert html_response(conn, 200) =~ "Listing Payment methods"
-    end
-  end
-
-  describe "new payment_method" do
+  describe "#new" do
     test "renders form", %{conn: conn} do
       conn = get conn, payment_method_path(conn, :new)
-      assert html_response(conn, 200) =~ "New Payment method"
+      assert html_response(conn, 200) =~ "New Payment Method"
     end
   end
 
-  describe "create payment_method" do
-    test "redirects to show when data is valid", %{conn: conn} do
-      conn = post conn, payment_method_path(conn, :create), payment_method: @create_attrs
+  describe "#create" do
+    test "redirects to billing#index when data is valid",
+      %{conn: conn, user: user} do
+      attrs = params_for(:payment_method, %{name: "some name"})
+      conn = conn
+      |> post(payment_method_path(conn, :create), payment_method: attrs)
 
-      assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == payment_method_path(conn, :show, id)
-
-      conn = get conn, payment_method_path(conn, :show, id)
-      assert html_response(conn, 200) =~ "Show Payment method"
+      assert redirected_to(conn) == billing_path(conn, :index)
+      payment_methods = Billing.list_payment_methods(user)
+      assert length(payment_methods) == 1
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post conn, payment_method_path(conn, :create), payment_method: @invalid_attrs
-      assert html_response(conn, 200) =~ "New Payment method"
+      attrs = %{stripe_id: nil}
+      conn = conn
+      |> post(payment_method_path(conn, :create), payment_method: attrs)
+      assert html_response(conn, 200) =~ "New Payment Method"
     end
   end
 
-  describe "edit payment_method" do
+  describe "#delete" do
     setup [:create_payment_method]
 
-    test "renders form for editing chosen payment_method", %{conn: conn, payment_method: payment_method} do
-      conn = get conn, payment_method_path(conn, :edit, payment_method)
-      assert html_response(conn, 200) =~ "Edit Payment method"
-    end
-  end
-
-  describe "update payment_method" do
-    setup [:create_payment_method]
-
-    test "redirects when data is valid", %{conn: conn, payment_method: payment_method} do
-      conn = put conn, payment_method_path(conn, :update, payment_method), payment_method: @update_attrs
-      assert redirected_to(conn) == payment_method_path(conn, :show, payment_method)
-
-      conn = get conn, payment_method_path(conn, :show, payment_method)
-      assert html_response(conn, 200) =~ "some updated last4"
+    test "deletes user's payment_method",
+      %{conn: conn, payment_method: payment_method, user: user} do
+      conn = delete(conn, payment_method_path(conn, :delete, payment_method))
+      assert redirected_to(conn) == billing_path(conn, :index)
+      assert_raise Ecto.NoResultsError, fn ->
+        Billing.get_payment_method!(user, payment_method.id) end
     end
 
-    test "renders errors when data is invalid", %{conn: conn, payment_method: payment_method} do
-      conn = put conn, payment_method_path(conn, :update, payment_method), payment_method: @invalid_attrs
-      assert html_response(conn, 200) =~ "Edit Payment method"
-    end
-  end
-
-  describe "delete payment_method" do
-    setup [:create_payment_method]
-
-    test "deletes chosen payment_method", %{conn: conn, payment_method: payment_method} do
-      conn = delete conn, payment_method_path(conn, :delete, payment_method)
-      assert redirected_to(conn) == payment_method_path(conn, :index)
+    test "does not delete other user's payment_method", %{conn: conn} do
+      payment_method = insert(:payment_method)
       assert_error_sent 404, fn ->
-        get conn, payment_method_path(conn, :show, payment_method)
+        delete(conn, payment_method_path(conn, :delete, payment_method))
       end
     end
   end
 
-  defp create_payment_method(_) do
-    payment_method = fixture(:payment_method)
-    {:ok, payment_method: payment_method}
+  def create_payment_method(%{user: user}) do
+    {:ok, payment_method: insert(:payment_method, user: user)}
   end
 end
