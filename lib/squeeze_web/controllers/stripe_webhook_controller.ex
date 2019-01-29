47ee97f79a13d796d857bafc83177149e8d688bf
Evan Dancer <evancdancer@gmail.com>
2019-01-29 08:00:17 -0800

    Add stripe webhook tests

--- lib/squeeze_web/controllers/stripe_webhook_controller.ex
+++ lib/squeeze_web/controllers/stripe_webhook_controller.ex
@@ -11,15 +11,34 @@ defmodule SqueezeWeb.StripeWebhookController do
   @secret Application.get_env(:stripity_stripe, :webhook_secret)
 
   def webhook(conn, _params) do
-    payload = conn.assigns.raw_body
-    [signature | _] = Conn.get_req_header(conn, "stripe-signature")
-    case Webhook.construct_event(payload, signature, @secret) do
+    payload = conn.assigns[:raw_body]
+    signature = get_stripe_signature(conn)
+    case construct_event(payload, signature) do
       {:ok, %Stripe.Event{} = _event} ->
         render(conn, "success.json")
       {:error, _reason} ->
-        render(conn, "success.json")
+        render_bad_request(conn)
     end
+  end
 
-    render(conn, "success.json")
+  defp get_stripe_signature(conn) do
+    case Conn.get_req_header(conn, "stripe-signature") do
+      [] -> nil
+      [signature | _] -> signature
+    end
+  end
+
+  defp construct_event(payload, signature) do
+    if is_nil(payload) || is_nil(signature) do
+      {:error, "invalid event"}
+    else
+      Webhook.construct_event(payload, signature, @secret)
+    end
+  end
+
+  defp render_bad_request(conn)  do
+    conn
+    |> put_status(:bad_request)
+    |> render("400.json")
   end
 end
---
+++ test/squeeze_web/controllers/stripe_webhook_controller_test.exs
@@ -0,0 +1,27 @@
+defmodule SqueezeWeb.StripeWebhookControllerTest do
+  use SqueezeWeb.ConnCase
+
+  describe "POST /webhook/stripe" do
+    test "returns 400 without a raw body", %{conn: conn} do
+      conn = conn
+      |> put_req_header("stripe-signature", "abcd")
+      |> post("/webhook/stripe")
+      assert json_response(conn, 400) == %{}
+    end
+
+    test "returns 400 without a stripe signature", %{conn: conn} do
+      conn = conn
+      |> assign(:raw_body, "")
+      |> post("/webhook/stripe")
+      assert json_response(conn, 400) == %{}
+    end
+
+    # test "returns 200 and empty response", %{conn: conn} do
+    #   conn = conn
+    #   |> assign(:raw_body, "")
+    # |> put_req_header("stripe-signature", "abcd")
+    #   |> post("/webhook/stripe")
+    #   assert json_response(conn, 200) == %{}
+    # end
+  end
+end
