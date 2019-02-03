338075d4b27765dd1cce4962a9d21eca28897727
Evan Dancer <evancdancer@gmail.com>
2019-02-02 23:30:11 -0800

    Add terms and privacy policy

--- lib/squeeze_web/views/page_view.ex
+++ lib/squeeze_web/views/page_view.ex
@@ -11,4 +11,8 @@ defmodule SqueezeWeb.PageView do
     Distances.distances
     |> Enum.map(fn(x) -> {x.name, x.distance} end)
   end
+
+  def company_name, do: "Squeeze.Run"
+  def website_name, do: "Squeeze.Run"
+  def website_url, do: "https://squeeze.run"
 end
