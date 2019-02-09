58695e178a525658082b6932e61a840acbd374ec
Evan Dancer <evancdancer@gmail.com>
2019-02-09 12:56:52 -0800

    Only show modal if user is not registered

--- lib/squeeze_web/controllers/page_controller.ex
+++ lib/squeeze_web/controllers/page_controller.ex
@@ -2,13 +2,12 @@ defmodule SqueezeWeb.PageController do
   use SqueezeWeb, :controller
 
   alias Squeeze.Accounts
-  alias Squeeze.Accounts.User
 
   def index(conn, _params) do
     user = conn.assigns.current_user
     changeset = Accounts.change_user_prefs(user.user_prefs)
 
-    case User.onboarded?(user) do
+    case user.registered do
       true -> redirect(conn, to: dashboard_path(conn, :index))
       false -> render(conn, "index.html", changeset: changeset, step: "distance")
     end
