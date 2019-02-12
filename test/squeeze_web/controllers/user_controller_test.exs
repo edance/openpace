464c2e3b57bcc92d22bef15487cab281974450f1
Evan Dancer <evancdancer@gmail.com>
2019-02-11 19:06:24 -0800

    Add payment_processor to user_controller tests

--- test/squeeze_web/controllers/user_controller_test.exs
+++ test/squeeze_web/controllers/user_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule SqueezeWeb.UserControllerTest do
   use SqueezeWeb.ConnCase
 
+  import Mox
+
   alias Squeeze.Accounts
 
   describe "GET /sign-up" do
@@ -21,11 +23,44 @@ defmodule SqueezeWeb.UserControllerTest do
   end
 
   @tag :guest_user
-  test "PUT /sign-up", %{conn: conn, user: user} do
+  describe "PUT /sign-up" do
+    setup [:setup_mocks]
+
+    test "updates the users email and password", %{conn: conn, user: user} do
+      register_user(conn)
+      user = Accounts.get_user!(user.id)
+      assert user.email == "test@example.com"
+      refute is_nil(user.encrypted_password)
+    end
+
+    test "creates customer and subscription", %{conn: conn, user: user} do
+      register_user(conn)
+      user = Accounts.get_user!(user.id)
+      refute is_nil(user.customer_id)
+      refute is_nil(user.subscription_id)
+    end
+  end
+
+  defp register_user(conn) do
     attrs = %{email: "test@example.com", encrypted_password: "password"}
     conn
     |> put(user_path(conn, :register), user: attrs)
+  end
 
-    assert Accounts.get_user!(user.id).email == "test@example.com"
+  defp setup_mocks(_) do
+    customer = %{
+      id: "customer_123456789"
+    }
+    subscription = %{
+      id: "sub_123456789"
+    }
+
+    Squeeze.MockPaymentProcessor
+    |> expect(:create_customer, fn(_) -> {:ok, customer} end)
+
+    Squeeze.MockPaymentProcessor
+    |> expect(:create_subscription, fn(_, _, _) -> {:ok, subscription} end)
+
+    {:ok, []}
   end
 end
