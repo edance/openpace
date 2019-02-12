bcb869b2e59124d0ffadd978ea5a642909fae912
Evan Dancer <evancdancer@gmail.com>
2019-02-11 18:56:48 -0800

    Add callbacks for new payment_processor methods

--- lib/squeeze/payment_processor/payment_processor.ex
+++ lib/squeeze/payment_processor/payment_processor.ex
@@ -4,7 +4,9 @@ defmodule Squeeze.PaymentProcessor do
   """
 
   @callback create_card(map()) :: {:ok, struct()} | {:error, struct()}
-
-  @callback create_product(map()) :: {:ok, struct()} | {:error, struct()}
+  @callback create_customer(map()) :: {:ok, struct()} | {:error, struct()}
   @callback create_plan(map()) :: {:ok, struct()} | {:error, struct()}
+  @callback create_product(map()) :: {:ok, struct()} | {:error, struct()}
+  @callback create_subscription(String.t, String.t, integer) ::
+  {:ok, struct()} | {:error, struct()}
 end
