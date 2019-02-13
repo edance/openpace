f68a42a38a89245cc9908e7d85bb58162d9ae81c
Evan Dancer <evancdancer@gmail.com>
2019-02-13 07:33:50 -0800

    Add tests for fitbit webhook verify

--- test/squeeze_web/controllers/fitbit_webhook_controller_test.exs
+++ test/squeeze_web/controllers/fitbit_webhook_controller_test.exs
@@ -2,5 +2,18 @@ defmodule SqueezeWeb.FitbitWebhookControllerTest do
   use SqueezeWeb.ConnCase
 
   describe "GET /webhook" do
+    test "verify with correct verify_token", %{conn: conn} do
+      conn = conn
+      |> get(fitbit_webhook_path(conn, :webhook), verify: "FITBIT")
+
+      assert response(conn, 204) == ""
+    end
+
+    test "verify with incorrect verify_token", %{conn: conn} do
+      conn = conn
+      |> get(fitbit_webhook_path(conn, :webhook), verify: "1234")
+
+      assert json_response(conn, 404) == %{}
+    end
   end
 end
