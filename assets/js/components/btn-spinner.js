8811bf4a2aaf8389a3a3fb104542f82e8242a746
Evan Dancer <evancdancer@gmail.com>
2019-02-20 21:08:18 -0800

    Set width for btn-spinner

--- assets/js/components/btn-spinner.js
+++ assets/js/components/btn-spinner.js
@@ -3,7 +3,9 @@ import { u } from 'umbrellajs';
 u(document).on('submit', 'form', function(event) {
   const $form = u(this);
   const $button = $form.find('.btn-spinner');
+  const width = $button.size().width;
   $button.attr('disabled', true);
+  $button.attr('style', `width: ${width}px`);
   $button.html(`
     <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
     <span class="sr-only">Loading...</span>
