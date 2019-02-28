51c7cf45ad449bee02dbd845dfe7a1c27624057c
Evan Dancer <evancdancer@gmail.com>
2019-02-27 18:58:03 -0800

    Refactor reset password controllers

--- lib/squeeze_web/controllers/forgot_password_controller.ex
+++ lib/squeeze_web/controllers/forgot_password_controller.ex
@@ -1,9 +1,7 @@
 defmodule SqueezeWeb.ForgotPasswordController do
   use SqueezeWeb, :controller
 
-  alias Squeeze.{Accounts, Crypto, Email, Mailer}
-
-  @base_url System.get_env("HOST_URL")
+  alias Squeeze.{Accounts, Email, Mailer, PasswordLinkGenerator}
 
   def show(conn, _params) do
     render(conn, "show.html")
@@ -12,17 +10,10 @@ defmodule SqueezeWeb.ForgotPasswordController do
   def request(conn, params) do
     user = Accounts.get_user_by_email(params["email"])
     user
-    |> Email.reset_password_email(reset_password_link(user))
+    |> Email.reset_password_email(PasswordLinkGenerator.create_link(user))
     |> Mailer.deliver_now()
     conn
     |> put_flash(:info, "Link sent to email")
     |> redirect(to: page_path(conn, :index))
-  end
-
-  defp reset_password_link(user) do
-    current_time = to_string(:erlang.system_time(:seconds))
-    reset_string = Base.url_encode64("#{current_time},#{user.id}")
-    signature = Crypto.sign(reset_string)
-    "#{@base_url}/reset-password?token=#{reset_string}&signature=#{signature}"
   end
 end
--- lib/squeeze_web/controllers/reset_password_controller.ex
+++ lib/squeeze_web/controllers/reset_password_controller.ex
@@ -3,22 +3,18 @@ defmodule SqueezeWeb.ResetPasswordController do
 
   alias Squeeze.Accounts
   alias Squeeze.Accounts.User
-  alias Squeeze.Crypto
-
-  @token_ttl 86_400
+  alias Squeeze.PasswordLinkGenerator
 
   plug :validate_token
 
   def show(conn, %{"token" => token, "signature" => signature}) do
-    [_, user_id] = parse_token(token)
-    user = Accounts.get_user!(user_id)
     changeset = Accounts.change_user(%User{})
     render(conn, "show.html", changeset: changeset,
-      signature: signature, token: token, user: user)
+      signature: signature, token: token)
   end
 
   def reset(conn, %{"token" => token, "signature" => signature, "user" => params}) do
-    [_, user_id] = parse_token(token)
+    [_, user_id] = PasswordLinkGenerator.parse_token(token)
     user = Accounts.get_user!(user_id)
     case Accounts.update_user_password(user, params) do
       {:ok, _} ->
@@ -34,29 +30,12 @@ defmodule SqueezeWeb.ResetPasswordController do
   defp validate_token(conn, _) do
     token = conn.params["token"]
     signature = conn.params["signature"]
-    case check_token(token, signature) do
+    case PasswordLinkGenerator.verify_link(token, signature) do
       {:ok, _} -> conn
       {:error, error_msg} ->
         conn
         |> put_flash(:error, error_msg)
         |> redirect(to: page_path(conn, :index))
     end
-  end
-
-  defp check_token(token, signature) do
-    [timestamp, _] = parse_token(token)
-    diff = :erlang.system_time(:seconds) - timestamp
-    if diff > @token_ttl do
-      {:error, "Token has expired"}
-    else
-      Crypto.verify(token, signature)
-    end
-  end
-
-  defp parse_token(token) do
-    token
-    |> Base.url_decode64!()
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
   end
 end
