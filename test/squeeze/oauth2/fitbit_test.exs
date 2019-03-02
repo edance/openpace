9027766c74eeb53ba3c840b6e471f4c716354d3f
Evan Dancer <evancdancer@gmail.com>
2019-03-01 21:42:13 -0800

    Add test for authorize_url/1

--- test/squeeze/oauth2/fitbit_test.exs
+++ test/squeeze/oauth2/fitbit_test.exs
@@ -1,3 +1,11 @@
 defmodule Squeeze.OAuth2.FitbitTest do
   use Squeeze.DataCase
+
+  alias Squeeze.OAuth2.Fitbit
+
+  describe "authorize_url/1" do
+    test "includes www.fitbit.com" do
+      assert Fitbit.authorize_url! =~ ~r/https:\/\/www.fitbit.com/
+    end
+  end
 end
--- test/squeeze/oauth2/google_test.exs
+++ test/squeeze/oauth2/google_test.exs
@@ -1,3 +1,11 @@
 defmodule Squeeze.OAuth2.GoogleTest do
   use Squeeze.DataCase
+
+  alias Squeeze.OAuth2.Google
+
+  describe "authorize_url/1" do
+    test "includes accounts.google.com" do
+      assert Google.authorize_url! =~ ~r/https:\/\/accounts.google.com/
+    end
+  end
 end
