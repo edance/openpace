1312c0bcb47fe6d1a1ad6691cb3a4e0d0e938013
Evan Dancer <evancdancer@gmail.com>
2019-03-04 21:04:37 -0800

    Add gzip for static assets

--- lib/squeeze_web/endpoint.ex
+++ lib/squeeze_web/endpoint.ex
@@ -8,7 +8,7 @@ defmodule SqueezeWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :squeeze, gzip: false,
+    at: "/", from: :squeeze, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
