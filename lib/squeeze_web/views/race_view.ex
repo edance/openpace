5cc65ceb199e63974286199d979e82b649760a84
Evan Dancer <evancdancer@gmail.com>
2019-03-20 20:11:37 -0700

    Add basic race page

--- lib/squeeze_web/views/race_view.ex
+++ lib/squeeze_web/views/race_view.ex
@@ -2,4 +2,10 @@ defmodule SqueezeWeb.RaceView do
   use SqueezeWeb, :view
 
   def title(_page, %{race: race}), do: race.name
+
+  def h1(%{race: race}), do: race.name
+
+  def location(%{race: race}) do
+    "#{race.city}, #{race.state}, #{race.country}"
+  end
 end
