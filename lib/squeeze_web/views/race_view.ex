fece08a11bf6d5cefb36b47b749026aa01ededbb
Evan Dancer <evancdancer@gmail.com>
2019-03-23 11:41:53 -0700

    Add countdown timer to race

--- lib/squeeze_web/views/race_view.ex
+++ lib/squeeze_web/views/race_view.ex
@@ -15,6 +15,22 @@ defmodule SqueezeWeb.RaceView do
     |> Timex.format!("%a %b #{Ordinal.ordinalize(start_at.day)}, %Y", :strftime)
   end
 
+  def start_at(%{race: race}) do
+    race.start_at
+    |> Timex.to_datetime(race.timezone)
+    |> DateTime.to_iso8601()
+  end
+
+  def countdown_timer(%{race: race}) do
+    date = Timex.to_datetime(race.start_at, race.timezone)
+    distance = Timex.diff(date, Timex.now, :seconds)
+    days = trunc(distance / (60 * 60 * 24))
+    hours = trunc(rem(distance, (60 * 60 * 24)) / (60 * 60))
+    minutes = trunc(rem(distance, (60 * 60)) / 60)
+    seconds = rem(distance, 60)
+    "#{days}d #{hours}h #{minutes}m #{seconds}s"
+  end
+
   def distance_type(%{race: race}) do
     race.distance_type
     |> Atom.to_string()
