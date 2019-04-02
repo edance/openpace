51372b1486cc3766773c9b8a4a730322f293d83d
Evan Dancer <evancdancer@gmail.com>
2019-04-01 21:57:06 -0700

    Update url and company name

--- lib/squeeze_web/views/layout_view.ex
+++ lib/squeeze_web/views/layout_view.ex
@@ -1,8 +1,8 @@
 defmodule SqueezeWeb.LayoutView do
   use SqueezeWeb, :view
 
-  def company_name, do: "Squeeze.Run"
+  def company_name, do: "OpenPace"
   def copyright_year, do: Date.utc_today.year
-  def website_name, do: "Squeeze"
-  def website_url, do: "https://squeeze.run"
+  def website_name, do: "OpenPace.co"
+  def website_url, do: "https://www.openpace.co"
 end
--- lib/squeeze_web/views/page_view.ex
+++ lib/squeeze_web/views/page_view.ex
@@ -6,5 +6,5 @@ defmodule SqueezeWeb.PageView do
 
   def company_name, do: "OpenPace"
   def website_name, do: "OpenPace.co"
-  def website_url, do: "https://openpace.co"
+  def website_url, do: "https://www.openpace.co"
 end
