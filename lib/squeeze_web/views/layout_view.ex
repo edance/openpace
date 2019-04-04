b5f90ef84a13f5e3e0cfe0b10ef831794749f88b
Evan Dancer <evancdancer@gmail.com>
2019-04-03 21:38:41 -0700

    Use openpace.co for sending emails

--- lib/squeeze_web/views/layout_view.ex
+++ lib/squeeze_web/views/layout_view.ex
@@ -3,6 +3,6 @@ defmodule SqueezeWeb.LayoutView do
 
   def company_name, do: "OpenPace"
   def copyright_year, do: Date.utc_today.year
-  def website_name, do: "OpenPace.co"
+  def website_name, do: "OpenPace"
   def website_url, do: "https://www.openpace.co"
 end
