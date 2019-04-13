86190951d1f931dffa360e7e8f3b32a6ef2244c6
Evan Dancer <evancdancer@gmail.com>
2019-04-12 21:52:20 -0700

    Add hidden input for date

--- assets/js/components/btn-calendar-activity.js
+++ assets/js/components/btn-calendar-activity.js
@@ -1,5 +1,7 @@
 import { u } from 'umbrellajs';
 
 u(document).on('click', '.btn-calendar-activity', function(event) {
+  const date = u(event.currentTarget).data('date');
+  u('#activity-modal .date').attr('value', date);
   $('#activity-modal').modal('show');
 });
