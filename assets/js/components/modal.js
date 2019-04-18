f66f18a6414e1ff6445621051f847f631a023185
Evan Dancer <evancdancer@gmail.com>
2019-04-17 20:53:05 -0700

    Add trial ended modal to billing
    Closes #238

--- assets/js/components/modal.js
+++ assets/js/components/modal.js
@@ -1,3 +1,7 @@
 document.addEventListener("turbolinks:load", function() {
   setTimeout(() => $('.modal[data-show="true"]').modal('show'), 1000);
 });
+
+document.addEventListener('turbolinks:before-visit', function() {
+  $('.modal').modal('hide');
+});
