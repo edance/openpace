d630248d66261f07343dfd5bbb8803ce78c6dc3c
Evan Dancer <evancdancer@gmail.com>
2019-04-22 20:22:48 -0700

    Add billing invoices

---
+++ priv/repo/migrations/20190423031111_create_billing_invoices.exs
@@ -0,0 +1,18 @@
+defmodule Squeeze.Repo.Migrations.CreateBillingInvoices do
+  use Ecto.Migration
+
+  def change do
+    create table(:billing_invoices) do
+      add :name, :string
+      add :amount_due, :integer
+      add :status, :string
+      add :date, :date
+      add :provider_id, :string
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:billing_invoices, [:provider_id])
+  end
+end
