b34f898ace68273112d98d527a8cb40471d2fea5
Evan Dancer <evancdancer@gmail.com>
2019-04-23 20:16:21 -0700

    Fix billing controller tests

--- test/squeeze_web/controllers/billing_controller_test.exs
+++ test/squeeze_web/controllers/billing_controller_test.exs
@@ -2,19 +2,12 @@ defmodule SqueezeWeb.BillingControllerTest do
   use SqueezeWeb.ConnCase
 
   describe "GET /" do
-    test "renders the title", %{conn: conn} do
-      conn = conn
-      |> get(billing_path(conn, :index))
-
-      assert html_response(conn, 200) =~ ~r/billing overview/i
-    end
-
     test "renders the user's payment method", %{conn: conn, user: user} do
       payment_method = insert(:payment_method, user: user)
       conn = conn
       |> get(billing_path(conn, :index))
 
-      assert html_response(conn, 200) =~ payment_method.owner_name
+      assert html_response(conn, 200) =~ payment_method.last4
     end
   end
 end
