4fa085fd292c4298cb3e262eb9711e0c6b890ac8
Evan Dancer <evancdancer@gmail.com>
2019-04-23 20:15:35 -0700

    Add reference to current plan for user on billing

--- lib/squeeze_web/controllers/billing_controller.ex
+++ lib/squeeze_web/controllers/billing_controller.ex
@@ -5,9 +5,11 @@ defmodule SqueezeWeb.BillingController  do
 
   def index(conn, _params) do
     user = conn.assigns.current_user
+    plan = Billing.get_default_plan()
     invoices = Billing.list_invoices(user)
     payment_method = Billing.get_default_payment_method(user)
-    render(conn, "index.html", invoices: invoices, payment_method: payment_method)
+    render(conn, "index.html",
+      invoices: invoices, payment_method: payment_method, plan: plan)
   end
 
   def cancel(conn, _params) do
