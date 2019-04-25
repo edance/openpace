afd6dd3ab446acb8d2a910c48254bee548c6d46e
Evan Dancer <evancdancer@gmail.com>
2019-04-24 21:00:43 -0700

    Add integration state to profile page

--- lib/squeeze_web/controllers/profile_controller.ex
+++ lib/squeeze_web/controllers/profile_controller.ex
@@ -9,17 +9,20 @@ defmodule SqueezeWeb.ProfileController  do
   end
 
   def edit(conn, _params, current_user) do
+    credentials = Accounts.list_credentials(current_user)
     changeset = Accounts.change_user(current_user)
-    render(conn, "edit.html", changeset: changeset)
+    render(conn, "edit.html", changeset: changeset, credentials: credentials)
   end
 
   def update(conn, %{"user" => user_params}, current_user) do
     case Accounts.update_user(current_user, user_params) do
       {:ok, _} ->
         conn
+        |> put_flash(:info, gettext("Your preferences have been updated"))
         |> redirect(to: dashboard_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", changeset: changeset)
+        credentials = Accounts.list_credentials(current_user)
+        render(conn, "edit.html", changeset: changeset, credentials: credentials)
     end
   end
 end
