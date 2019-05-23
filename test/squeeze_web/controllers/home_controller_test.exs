920f93ae66a09b8fd764f2a0ad191a8d66ccebf4
Evan Dancer <evancdancer@gmail.com>
2019-05-22 15:02:30 -1000

    Add tests for email subscribe

--- test/squeeze_web/controllers/home_controller_test.exs
+++ test/squeeze_web/controllers/home_controller_test.exs
@@ -15,4 +15,23 @@ defmodule SqueezeWeb.HomeControllerTest do
       assert redirected_to(conn) == dashboard_path(conn, :index)
     end
   end
+
+  describe "POST /" do
+    @tag :guest_user
+    test "with a valid email address", %{conn: conn} do
+      conn = conn
+      |> post("/", subscription: %{"email" => "test@test.com"})
+
+      assert get_flash(conn, :info) == "Thanks for signing up!"
+      assert redirected_to(conn) == home_path(conn, :index)
+    end
+
+    @tag :guest_user
+    test "with an invalid email", %{conn: conn} do
+      conn = conn
+      |> post("/", subscription: %{"email" => "test"})
+
+      assert get_flash(conn, :error) == "Invalid email address"
+    end
+  end
 end
