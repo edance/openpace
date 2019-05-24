03e2562cf15fc288038bab77989e84595d4a33c4
Evan Dancer <evancdancer@gmail.com>
2019-05-23 22:02:12 -0700

    Add tests for billing#cancel

--- test/squeeze_web/controllers/billing_controller_test.exs
+++ test/squeeze_web/controllers/billing_controller_test.exs
@@ -1,6 +1,8 @@
 defmodule SqueezeWeb.BillingControllerTest do
   use SqueezeWeb.ConnCase
 
+  import Mox
+
   describe "GET /" do
     test "renders the user's payment method", %{conn: conn, user: user} do
       payment_method = insert(:payment_method, user: user)
@@ -9,5 +11,25 @@ defmodule SqueezeWeb.BillingControllerTest do
 
       assert html_response(conn, 200) =~ payment_method.last4
     end
+  end
+
+  describe "PUT /cancel" do
+    setup [:setup_mocks]
+
+    @tag :paid_user
+    test "redirects and shows flash message", %{conn: conn} do
+      conn = conn
+      |> put(billing_path(conn, :cancel))
+
+      assert get_flash(conn, :info) == "Your membership has been canceled"
+      assert redirected_to(conn) == billing_path(conn, :index)
+    end
+  end
+
+  defp setup_mocks(_) do
+    Squeeze.MockPaymentProcessor
+    |> expect(:cancel_subscription, fn(_) -> {:ok, %{}} end)
+
+    {:ok, []}
   end
 end
