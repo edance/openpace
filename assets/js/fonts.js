bbf8fdc41917e6004fe49bf221d1ea87b232a728
Evan Dancer <evancdancer@gmail.com>
2019-05-26 14:36:41 -0700

    Remove simple line icons

--- assets/js/fonts.js
+++ assets/js/fonts.js
@@ -2,10 +2,6 @@
 import WebFont from 'webfontloader';
 
 WebFont.load({
-  custom: {
-    families: ['simple-line-icons'],
-    urls: ['//cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css'],
-  },
   google: {
     families: ['Open Sans'],
   },
