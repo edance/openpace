ca52a962d79bcb4ef4b735aa721e5b491287e6ee
Evan Dancer <evancdancer@gmail.com>
2019-05-26 14:29:07 -0700

    Remove YoungSerif and BreeSerif

--- assets/js/fonts.js
+++ assets/js/fonts.js
@@ -7,7 +7,7 @@ WebFont.load({
     urls: ['//cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css'],
   },
   google: {
-    families: ['Open Sans:300,400', 'Material Icons'],
+    families: ['Open Sans'],
   },
 });
 
