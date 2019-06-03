16f415e1358e472b9120c476b1357e8f3bc44c5d
Evan Dancer <evancdancer@gmail.com>
2019-06-02 19:14:41 -0700

    Bump elixir from 1.6 to 1.7

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Squeeze.Mixfile do
     [
       app: :squeeze,
       version: "0.0.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
