dc0e7f16cb53a433af9428c11a9255ec000c95ef
Evan Dancer <evancdancer@gmail.com>
2019-06-18 21:17:51 -0700

    Fix issues with fitbit activity loader

--- lib/squeeze/fitbit/activity_loader.ex
+++ lib/squeeze/fitbit/activity_loader.ex
@@ -9,7 +9,7 @@ defmodule Squeeze.Fitbit.ActivityLoader do
 
   def update_or_create_activity(%Credential{} = credential, fitbit_activity) do
     user = credential.user
-    activity = map_activity(user, fitbit_activity)
+    activity = map_activity(fitbit_activity)
 
     case ActivityMatcher.get_closest_activity(user, activity) do
       nil -> Dashboard.create_activity(user, activity)
@@ -19,18 +19,32 @@ defmodule Squeeze.Fitbit.ActivityLoader do
     end
   end
 
-  defp map_activity(user, activity) do
+  defp map_activity(%{"activityName" => name, "logId" => id} = activity) do
     %{
-      name: activity["name"],
-      type: String.downcase(activity["activityParentName"]),
-      distance: activity["distance"],
-      duration: activity["duration"],
-      start_at: start_at(user, activity),
-      external_id: "#{activity["activityId"]}"
+      name: name,
+      type: name,
+      distance: distance(activity),
+      duration: duration(activity),
+      start_at: start_at(activity),
+      external_id: "#{id}"
     }
   end
 
-  defp start_at(_user, %{"startDate" => _date, "startTime" => _time}) do
-    Timex.now()
+  defp start_at(%{"startTime" => start_time}) do
+    case Timex.parse(start_time, "{ISO:Extended:Z}") do
+      {:ok, t} -> t
+      _ -> nil
+    end
   end
+
+  def distance(%{"distance" => distance, "distanceUnit" => unit}) do
+    case unit do
+      "Kilometer" -> distance * 1000.0
+      _ -> distance
+    end
+  end
+  def distance(_), do: 0
+
+  def duration(%{"duration" => duration}), do: trunc(duration / 1000)
+  def duration(_), do: 0
 end
