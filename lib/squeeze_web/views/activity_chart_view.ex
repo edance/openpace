e4d0d7aecbeca9982f2cd305f91f86a38b7d28e1
Evan Dancer <evancdancer@gmail.com>
2019-06-25 21:07:20 -0700

    Get data close to rendering

--- lib/squeeze_web/views/activity_chart_view.ex
+++ lib/squeeze_web/views/activity_chart_view.ex
@@ -1,3 +1,26 @@
 defmodule SqueezeWeb.ActivityChartView do
   use SqueezeWeb, :view
+
+  def pace(%{current_user: _user, trackpoints: trackpoints}) do
+    trackpoints
+    |> map_field(:velocity)
+    |> Jason.encode!()
+  end
+
+  def heartrate(%{current_user: _user, trackpoints: trackpoints}) do
+    trackpoints
+    |> map_field(:heartrate)
+    |> Jason.encode!()
+  end
+
+  def elevation(%{current_user: _user, trackpoints: trackpoints}) do
+    trackpoints
+    |> map_field(:altitude)
+    |> Jason.encode!()
+  end
+
+  defp map_field(trackpoints, field) do
+    trackpoints
+    |> Enum.map(&(%{x: Map.get(&1, :time), y: Map.get(&1, field)}))
+  end
 end
