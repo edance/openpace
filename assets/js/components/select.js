e5e41028697f9f204ec2b09fc091b3e56be51896
Evan Dancer <evancdancer@gmail.com>
2019-07-25 22:41:31 -0700

    Add select for event modal

--- assets/js/components/select.js
+++ assets/js/components/select.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactSelect from 'react-select';
 
 export default function Select(props) {
+  const [value, setValue] = useState(props.value);
+
   return (
-    <ReactSelect
-        className="basic-single"
-        classNamePrefix="select"
-        isClearable
-        {...props}
-    />
+    <>
+      <ReactSelect
+          isClearable
+          onChange={setValue}
+          value={value}
+          {...props}
+      />
+
+      { value && <input name={name} type="hidden" value={value.value} /> }
+    </>
   );
 }
