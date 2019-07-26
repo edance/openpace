5fe80dc16647acece5e288cc9cf340b235128a5a
Evan Dancer <evancdancer@gmail.com>
2019-07-25 22:41:31 -0700

    Add select for event modal

--- lib/squeeze_web/views/modal_view.ex
+++ lib/squeeze_web/views/modal_view.ex
@@ -1,3 +1,15 @@
 defmodule SqueezeWeb.ModalView do
   use SqueezeWeb, :view
+
+  def run_types do
+    [
+      %{value: "Run", label: "Run"},
+      %{value: "Bike", label: "Bike"},
+      %{value: "Swim", label: "Swim"},
+      %{value: "Cross Training", label: "Cross Training"},
+      %{value: "Walk", label: "Walk"},
+      %{value: "Strength Training", label: "Strength Training"},
+      %{value: "Workout", label: "Workout"}
+    ]
+  end
 end
