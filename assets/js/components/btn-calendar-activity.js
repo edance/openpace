ecec3cfcf944388e2463dcb9549e4dcb467c314c
Evan Dancer <evancdancer@gmail.com>
2019-07-30 20:50:04 -0700

    Select date when opening activity modal

--- assets/js/components/btn-calendar-activity.js
+++ assets/js/components/btn-calendar-activity.js
@@ -3,6 +3,6 @@ import { u } from 'umbrellajs';
 u(document).on('click', '.btn-calendar-activity', function(event) {
   const date = u(event.currentTarget).data('date');
   const $modal = u('#activity-modal');
-  u('#activity-modal .date').attr('value', date);
+  u('#activity-modal .date-picker').attr('value', date);
   $('#activity-modal').modal('show');
 });
