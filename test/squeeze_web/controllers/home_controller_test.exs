f0a1e63606a549ffba8ef685eaf6422a8ef10c71
Evan Dancer <evancdancer@gmail.com>
2019-08-14 19:58:32 -0700

    Redirect home to www or dashboard

--- test/squeeze_web/controllers/home_controller_test.exs
+++ test/squeeze_web/controllers/home_controller_test.exs
@@ -6,32 +6,13 @@ defmodule SqueezeWeb.HomeControllerTest do
     test "as a guest user", %{conn: conn} do
       conn = conn
       |> get("/")
-      assert html_response(conn, 200) =~ "OpenPace"
+      assert redirected_to(conn) == session_path(conn, :new)
     end
 
     test "as a complete user", %{conn: conn} do
       conn = conn
       |> get("/")
       assert redirected_to(conn) == dashboard_path(conn, :index)
-    end
-  end
-
-  describe "POST /" do
-    @tag :guest_user
-    test "with a valid email address", %{conn: conn} do
-      conn = conn
-      |> post("/", subscription: %{"email" => "test@test.com"})
-
-      assert get_flash(conn, :info) == "Thanks for signing up!"
-      assert redirected_to(conn) == home_path(conn, :index)
-    end
-
-    @tag :guest_user
-    test "with an invalid email", %{conn: conn} do
-      conn = conn
-      |> post("/", subscription: %{"email" => "test"})
-
-      assert get_flash(conn, :error) == "Invalid email address"
     end
   end
 end
