1b3729e3ddd43cd0cfda32beb5b3190df1bca341
Evan Dancer <evancdancer@gmail.com>
2019-08-27 12:24:16 -0700

    Update activity#show to work with planned activities

--- lib/squeeze_web/views/activity_view.ex
+++ lib/squeeze_web/views/activity_view.ex
@@ -20,6 +20,48 @@ defmodule SqueezeWeb.ActivityView do
     ]
   end
 
+  def name(%{activity: activity}) do
+    activity.name || activity.type
+  end
+
+  def duration(%{activity: activity}) do
+    cond do
+      activity.duration && activity.duration > 0 ->
+        format_duration(activity.duration)
+      activity.planned_duration && activity.planned_duration > 0 ->
+        format_duration(activity.planned_duration)
+      true ->
+        "N/A"
+    end
+  end
+
+  def distance(%{activity: activity, current_user: user}) do
+    cond do
+      activity.distance_amount && activity.distance_unit ->
+        "#{activity.distance_amount} #{activity.distance_unit}"
+      activity.distance && activity.distance > 0 ->
+        format_distance(activity.distance, user.user_prefs)
+      activity.planned_distance_amount && activity.planned_distance_unit ->
+        "#{activity.planned_distance_amount} #{activity.planned_distance_unit}"
+      true ->
+        "N/A"
+    end
+  end
+
+  def date(%{activity: %{start_at: nil, planned_date: date}}) do
+    Timex.format!(date, "%b %-d, %Y ", :strftime)
+  end
+  def date(%{activity: activity, current_user: user}) do
+    timezone = user.user_prefs.timezone
+    activity.start_at
+    |> Timex.to_datetime(timezone)
+    |> Timex.format!("%b %-d, %Y %-I:%M %p ", :strftime)
+  end
+
+  def trackpoints?(%{trackpoints: trackpoints}) do
+    length(trackpoints) > 0
+  end
+
   def splits(assigns) do
     trackpoints = split_trackpoints(assigns)
     trackpoints
