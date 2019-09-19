e3ff73971b877aa19543a832bcf57a0e22dde126
Evan Dancer <evancdancer@gmail.com>
2019-09-18 20:43:30 -0700

    Add activity chart with highcharts

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -6,7 +6,7 @@ import 'bootstrap';
 import './components/base';
 
 // Dashboard only components
-// import './components/activity-chart';
+import './components/activity-chart';
 import './components/activity-map';
 import './components/week-chart';
 import './components/year-chart';
