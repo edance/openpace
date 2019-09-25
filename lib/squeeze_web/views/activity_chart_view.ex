4a70d6342cd001686a937c65137c70fcda53d48f
Evan Dancer <evancdancer@gmail.com>
2019-09-24 20:19:53 -0700

    Add elevation, cadence, heart rate, and pace

--- lib/squeeze_web/views/activity_chart_view.ex
+++ lib/squeeze_web/views/activity_chart_view.ex
@@ -3,19 +3,47 @@ defmodule SqueezeWeb.ActivityChartView do
 
   alias Squeeze.{Distances, Velocity}
 
-  def json(%{trackpoints: trackpoints, current_user: user}) do
+  def altitude(%{trackpoints: trackpoints, current_user: user}) do
     imperial = user.user_prefs.imperial
     trackpoints
-    |> Enum.map(&(trackpoint(&1, imperial)))
+    |> Enum.map(&(Distances.to_feet(&1.altitude, imperial: imperial)))
+    |> smooth()
+    |> Jason.encode!()
   end
 
-  defp trackpoint(t, imperial) do
-    %{
-      altitude: Distances.to_feet(t.altitude, imperial: imperial),
-      cadence: t.cadence * 2,
-      distance: Distances.to_float(t.distance, imperial: imperial),
-      heartrate: t.heartrate,
-      velocity: Velocity.to_float(t.velocity, imperial: imperial)
-    }
+  def cadence(%{trackpoints: trackpoints}) do
+    trackpoints
+    |> Enum.map(&(&1.cadence * 2))
+    |> smooth()
+    |> Jason.encode!()
+  end
+
+  def distance(%{trackpoints: trackpoints, current_user: user}) do
+    imperial = user.user_prefs.imperial
+    trackpoints
+    |> Enum.map(&(Distances.to_float(&1.distance, imperial: imperial)))
+    |> smooth()
+    |> Jason.encode!()
+  end
+
+  def heartrate(%{trackpoints: trackpoints}) do
+    trackpoints
+    |> Enum.map(&(&1.heartrate))
+    |> smooth()
+    |> Jason.encode!()
+  end
+
+  def velocity(%{trackpoints: trackpoints, current_user: user}) do
+    imperial = user.user_prefs.imperial
+    trackpoints
+    |> Enum.map(&(Velocity.to_float(&1.velocity, imperial: imperial)))
+    |> smooth()
+    |> Jason.encode!()
+  end
+
+  defp smooth(points) do
+    points
+    |> SMA.sma(15)
+    |> Enum.map(&(Float.round(&1, 2)))
   end
 end
