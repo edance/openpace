11dc7f0f5215266b042ea8bb626df8c88eb7deaa
Evan Dancer <evancdancer@gmail.com>
2019-10-02 20:47:23 -0700

    Remove all unused db seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,36 +9,3 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-alias Faker.{Lorem}
-alias Squeeze.Repo
-alias Squeeze.Accounts
-alias Squeeze.TrainingPlans.{Plan, Event}
-
-{:ok, %{id: user_id}} = Accounts.create_guest_user()
-
-plan = %Plan{
-  name: "Advanced Marathon Training Plan",
-  experience_level: :advanced,
-  week_count: 18,
-  description: "",
-  user_id: user_id
-}
-%{id: plan_id} = Repo.insert!(plan)
-
-Enum.map(0..(18 * 7 - 1), fn(x) ->
-  dist = Enum.random(4..16)
-  pace = Enum.random(["easy", "tempo", "long run"])
-
-  event = %Event{
-    name: "#{dist} miles #{pace}",
-    type: "Run",
-    distance: dist * 1_609.0,
-    duration: nil,
-    warmup: false,
-    cooldown: false,
-    plan_position: x,
-    day_position: 0,
-    training_plan_id: plan_id
-  }
-  Repo.insert!(event)
-end)
