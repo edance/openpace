3987deaccb60b29a7f56e9a0a196be8724bdb6ea
Evan Dancer <evancdancer@gmail.com>
2019-10-02 20:42:39 -0700

    Add workout type to activities

--- lib/squeeze/ecto_enums.ex
+++ lib/squeeze/ecto_enums.ex
@@ -6,3 +6,4 @@ defenum ExperienceLevelEnum, novice: 0, intermediate: 1, advanced: 2
 defenum GenderEnum, male: 0, female: 1, other: 2, prefer_not_to_say: 3
 defenum SubscriptionStatusEnum, incomplete: 0, incomplete_expired: 1, trialing: 2,
   active: 3, past_due: 4, canceled: 5, unpaid: 6
+defenum WorkoutTypeEnum, race: 0, long_run: 1, workout: 2
