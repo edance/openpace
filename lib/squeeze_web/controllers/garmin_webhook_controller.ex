6e9ef24538cebf35d1070ff8067687e3aa2c75dd
Evan Dancer <evancdancer@gmail.com>
2019-10-28 20:33:18 -0700

    Add basic webhook controller for garmin
    Nothing happens currently but responds 200 with an empty json object.

---
+++ lib/squeeze_web/controllers/garmin_webhook_controller.ex
@@ -0,0 +1,21 @@
+defmodule SqueezeWeb.GarminWebhookController do
+  use SqueezeWeb, :controller
+
+  @moduledoc """
+  Controller to handle the pings from Garmin
+  """
+
+  alias Squeeze.Logger
+
+  plug :log_webhook_event
+
+  def webhook(conn, _params) do
+    render(conn, "success.json")
+  end
+
+  defp log_webhook_event(conn, _) do
+    body = Jason.encode!(conn.params)
+    Logger.log_webhook_event(%{provider: "garmin", body: body})
+    conn
+  end
+end
