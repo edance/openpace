67c10cab508cc6ae75bb95e830d6a9edab4fe8f6
Evan Dancer <evancdancer@gmail.com>
2020-05-20 20:14:49 -0700

    Add races from existing code

---
+++ lib/squeeze/races/race.ex
@@ -0,0 +1,38 @@
+defmodule Squeeze.Races.Race do
+  @moduledoc """
+  This module contains the schema for the race.
+  """
+
+  use Ecto.Schema
+
+  alias Squeeze.Races.{Event}
+
+  # @required_fields ~w(name slug city state country)a
+  # @optional_fields ~w(content url)a
+
+  schema "races" do
+    field :name, :string
+    field :slug, :string
+    field :content, :string
+
+    field :address_line1, :string
+    field :address_line2, :string
+    field :city, :string
+    field :state, :string
+    field :country, :string
+    field :postal_code, :string
+
+    field :logo_url, :string
+
+    field :timezone, :string
+
+    field :registration_url, :string
+    field :url, :string
+
+    field :external_id, :string
+
+    has_many :events, Event
+
+    timestamps()
+  end
+end
