01bde196b7c55e8fb0a16b16cfd950e10cc423db
Evan Dancer <evancdancer@gmail.com>
2020-05-29 21:41:27 -0700

    Add badges to races

--- lib/squeeze/ecto_enums.ex
+++ lib/squeeze/ecto_enums.ex
@@ -8,7 +8,6 @@ defenum SubscriptionStatusEnum, incomplete: 0, incomplete_expired: 1, trialing: 
   active: 3, past_due: 4, canceled: 5, unpaid: 6
 defenum WorkoutTypeEnum, race: 0, long_run: 1, workout: 2
 
-defenum CourseProfileEnum, downhill: 0, flat: 1, mostly_flat: 2, rolling_hills: 3,
-  hilly: 4, very_hilly: 5
-defenum CourseTerrainEnum, road: 0, mostly_road: 1, mostly_trail: 2, trail: 3
+defenum CourseProfileEnum, downhill: 0, flat: 1, rolling_hills: 2, hilly: 3
+defenum CourseTerrainEnum, road: 0, trail: 1
 defenum CourseTypeEnum, loop: 0, out_and_back: 1, point_to_point: 2
