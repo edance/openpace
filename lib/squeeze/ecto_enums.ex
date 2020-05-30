3164dd9ac812594870895c85fa33433190a64811
Evan Dancer <evancdancer@gmail.com>
2020-05-29 19:54:43 -0700

    Add course information to races

--- lib/squeeze/ecto_enums.ex
+++ lib/squeeze/ecto_enums.ex
@@ -7,3 +7,8 @@ defenum GenderEnum, male: 0, female: 1, other: 2, prefer_not_to_say: 3
 defenum SubscriptionStatusEnum, incomplete: 0, incomplete_expired: 1, trialing: 2,
   active: 3, past_due: 4, canceled: 5, unpaid: 6
 defenum WorkoutTypeEnum, race: 0, long_run: 1, workout: 2
+
+defenum CourseProfileEnum, downhill: 0, flat: 1, mostly_flat: 2, rolling_hills: 3,
+  hilly: 4, very_hilly: 5
+defenum CourseTerrainEnum, road: 0, mostly_road: 1, mostly_trail: 2, trail: 3
+defenum CourseTypeEnum, loop: 0, out_and_back: 1, point_to_point: 2
