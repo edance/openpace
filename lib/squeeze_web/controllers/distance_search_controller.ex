c806c48414d30938f8f5df3d9643e27e6e04a96e
Evan Dancer <evancdancer@gmail.com>
2020-06-07 13:23:39 -0700

    Refactor race search

--- lib/squeeze_web/controllers/distance_search_controller.ex
+++ lib/squeeze_web/controllers/distance_search_controller.ex
@@ -1,24 +1,14 @@
 defmodule SqueezeWeb.DistanceSearchController do
   use SqueezeWeb, :controller
 
-  def index(conn, %{"region" => region, "distance" => distance} = params) do
-    case find_races(params) do
+  alias Squeeze.RaceSearch
+
+  def index(conn, %{"region" => region, "distance" => distance}) do
+    case RaceSearch.search_distance_region(%{distance: distance, region: region}) do
       {:ok, results} ->
         render(conn, "index.html", region: region, distance: distance, results: results)
       _ ->
         render(conn, "index.html", region: region, distance: distance)
     end
-  end
-
-  defp find_races(params) do
-    Algolia.search("Race", "", [facetFilters: facet_filters(params), facets: facets()])
-  end
-
-  defp facet_filters(%{"region" => region, "distance" => _distance}) do
-    ["full_state:#{region}"]
-  end
-
-  defp facets do
-    ~w(full_state weekday month course_profile course_terrain course_type boston_qualifier)
   end
 end
