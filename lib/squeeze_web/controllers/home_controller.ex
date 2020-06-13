9b1f2c10e25227c9a85f11b287602aaee4da692b
Evan Dancer <evancdancer@gmail.com>
2020-06-12 19:29:47 -0700

    Add homepage and mailer subscription
    This reverts commit f1c16fe207bb093e8d77b45d46a9140dca35a5a4.

--- lib/squeeze_web/controllers/home_controller.ex
+++ lib/squeeze_web/controllers/home_controller.ex
@@ -1,14 +1,32 @@
 defmodule SqueezeWeb.HomeController do
   use SqueezeWeb, :controller
 
+  alias Squeeze.MailingList
+  alias Squeeze.MailingList.Subscription
+
   plug :redirect_registered_user
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    changeset = MailingList.change_subscription(%Subscription{})
+    render(conn, "index.html", changeset: changeset)
+  end
+
+  def subscribe(conn, %{"subscription" => subscription_params}) do
+    attrs = Map.merge(subscription_params, %{"type" => "homepage"})
+    case MailingList.create_subscription(attrs) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "Thanks for signing up!")
+        |> redirect(to: Routes.home_path(conn, :index))
+      {:error, %Ecto.Changeset{} = changeset} ->
+        conn
+        |> put_flash(:error, "Invalid email address")
+        |> render("index.html", changeset: changeset)
+    end
   end
 
   defp redirect_registered_user(conn, _) do
-    case conn.assigns.current_user do
+    case conn.assigns[:current_user] do
       nil -> conn
       _ ->
         conn
---
+++ lib/squeeze_web/views/home_view.ex
@@ -0,0 +1,5 @@
+defmodule SqueezeWeb.HomeView do
+  use SqueezeWeb, :view
+
+  def title(_, _), do: gettext("Run Your Best Race Ever")
+end
