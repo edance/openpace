a23055a76f2d43e0ee16ef7d2ad309b804bf2fd5
Evan Dancer <evancdancer@gmail.com>
2020-08-22 17:01:47 -0700

    Add more info to user

--- lib/squeeze_web/views/api/user_view.ex
+++ lib/squeeze_web/views/api/user_view.ex
@@ -1,7 +1,12 @@
 defmodule SqueezeWeb.Api.UserView do
   def render("user.json", %{user: user, token: token}) do
     %{
-      email: user.email,
+      user: %{
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        id: user.id
+      },
       token: token
     }
   end
