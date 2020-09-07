7ab7aa12b288a62aa71615f2394df5a1203072f6
Evan Dancer <evancdancer@gmail.com>
2020-09-07 15:01:29 -0700

    Add endpoints for challenge show and index

--- lib/squeeze_web/controllers/api/challenge_controller.ex
+++ lib/squeeze_web/controllers/api/challenge_controller.ex
@@ -5,20 +5,27 @@ defmodule SqueezeWeb.Api.ChallengeController do
 
   action_fallback SqueezeWeb.Api.FallbackController
 
-  def index(conn, _) do
-    render(conn, %{})
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns.current_user]
+    apply(__MODULE__, action_name(conn), args)
   end
 
-  def create(conn, %{"challenge" => params}) do
-    user = conn.assigns.current_user
+  def index(conn, _, user) do
+    challenges = Challenges.list_challenges(user)
 
+    render(conn, "index.json", %{challenges: challenges})
+  end
+
+  def show(conn, %{"id" => id}, user) do
+    challenge = Challenges.get_challenge!(user, id)
+    render(conn, "show.json", %{challenge: challenge})
+  end
+
+  def create(conn, %{"challenge" => params}, user) do
     with {:ok, challenge} <- Challenges.create_challenge(user, params) do
       conn
       |> put_status(:created)
       |> render("challenge.json", %{challenge: challenge})
     end
-  end
-
-  def update(_, _) do
   end
 end
--- lib/squeeze_web/views/api/challenge_view.ex
+++ lib/squeeze_web/views/api/challenge_view.ex
@@ -1,4 +1,12 @@
 defmodule SqueezeWeb.Api.ChallengeView do
+  def render("index.json", %{challenges: challenges}) do
+    %{challenges: render_many(challenges, SqueezeWeb.Api.ChallengeView, "challenge.json")}
+  end
+
+  def render("show.json", %{challenge: challenge}) do
+    render_one(challenge, SqueezeWeb.Api.ChallengeView, "challenge.json")
+  end
+
   def render("challenge.json", %{challenge: challenge}) do
     %{
       id: challenge.id,
