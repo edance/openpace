8577f8c5e73480759323eaba0117937eea74380a
Evan Dancer <evancdancer@gmail.com>
2020-10-27 20:47:51 -0700

    Load strava history after connecting

--- lib/squeeze_web/controllers/api/strava_controller.ex
+++ lib/squeeze_web/controllers/api/strava_controller.ex
@@ -12,6 +12,9 @@ defmodule SqueezeWeb.Api.StravaController do
     params = credential_params(client, athlete)
     with {:ok, credential} <- Accounts.create_credential(user, params),
          {:ok, _} <- Accounts.update_user(user, user_params(athlete)) do
+
+      load_activity_history(credential)
+
       conn
       |> put_status(:created)
       |> render("credential.json", %{credential: credential})
@@ -26,7 +29,7 @@ defmodule SqueezeWeb.Api.StravaController do
     @strava_auth.get_athlete!(client)
   end
 
-  def user_params(athlete) do
+  defp user_params(athlete) do
     %{
       avatar: athlete.profile,
       city: athlete.city,
@@ -35,7 +38,12 @@ defmodule SqueezeWeb.Api.StravaController do
     }
   end
 
-  def credential_params(%{token: token}, athlete) do
+  defp load_activity_history(%{provider: "strava", uid: id}) do
+    credential = Accounts.get_credential("strava", id)
+    Task.start(fn -> Squeeze.Strava.HistoryLoader.load_recent(credential) end)
+  end
+
+  defp credential_params(%{token: token}, athlete) do
     token
     |> Map.take([:access_token, :refresh_token])
     |> Map.merge(%{provider: "strava", uid: "#{athlete.id}"})
