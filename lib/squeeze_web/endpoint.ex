a7ac4026bf6bd540c9b6136e5fac612320079edb
Evan Dancer <evancdancer@gmail.com>
2020-11-22 12:48:24 -0800

    Add well-known files for deep links

--- lib/squeeze_web/endpoint.ex
+++ lib/squeeze_web/endpoint.ex
@@ -7,7 +7,7 @@ defmodule SqueezeWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :squeeze, gzip: true,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt .well-known)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
