ffd60b92671aa117b116e1d026f219ef8634c131
Evan Dancer <evancdancer@gmail.com>
2020-12-30 16:38:11 -0500

    Fix content type for apple-app-site-association

--- lib/squeeze_web/endpoint.ex
+++ lib/squeeze_web/endpoint.ex
@@ -7,7 +7,8 @@ defmodule SqueezeWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :squeeze, gzip: true,
-    only: ~w(css fonts images js favicon.ico robots.txt .well-known)
+    only: ~w(css fonts images js favicon.ico robots.txt .well-known),
+    content_types: %{"apple-app-site-association" => "application/json"}
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
