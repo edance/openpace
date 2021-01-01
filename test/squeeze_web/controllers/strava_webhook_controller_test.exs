65fe124c59b9793a32c41c2c71da60e5a4078ff5
Evan Dancer <evancdancer@gmail.com>
2021-01-01 15:04:46 -0500

    Add tests for strava webhook

--- test/squeeze_web/controllers/strava_webhook_controller_test.exs
+++ test/squeeze_web/controllers/strava_webhook_controller_test.exs
@@ -1,6 +1,13 @@
 defmodule SqueezeWeb.StravaWebhookControllerTest do
   use SqueezeWeb.ConnCase
 
+  @moduledoc false
+
+  import Squeeze.Accounts
+  import Squeeze.Dashboard
+  import Squeeze.Dashboard.Activity
+  import Squeeze.Factory
+
   describe "GET /webhook/strava" do
     test "returns 400 with no challenge", %{conn: conn} do
       conn = get(conn, "/webhook/strava")
@@ -21,9 +28,66 @@ defmodule SqueezeWeb.StravaWebhookControllerTest do
   end
 
   describe "POST /webhook/strava" do
+    setup [:setup_account]
+
     test "returns 200 and empty response", %{conn: conn} do
       conn = post(conn, "/webhook/strava")
       assert json_response(conn, 200) == %{}
     end
+
+    test "user deletes an activity on strava", %{conn: conn, activity: activity, credential: credential} do
+      params = %{
+        "aspect_type" => "delete",
+        "object_type" => "activity",
+        "object_id" => activity.external_id,
+        "owner_id" => credential.uid
+      }
+      conn = post(conn, "/webhook/strava", params)
+      assert json_response(conn, 200) == %{}
+      assert_raise Ecto.NoResultsError, fn ->
+        Dashboard.get_activity_by_external_id!(credential.user, activity.external_id) end
+    end
+
+    test "user creates an activity on strava", %{conn: conn, credential: credential} do
+      activity = build(:activity)
+      params = %{
+        "aspect_type" => "create",
+        "object_type" => "activity",
+        "object_id" => activity.external_id,
+        "owner_id" => credential.uid
+      }
+      conn = post(conn, "/webhook/strava", params)
+      assert %Activity{} = Dashboard.get_activity_by_external_id!(credential.user, activity.external_id)
+    end
+
+    test "user creates an activity on strava", %{conn: conn, credential: credential, activity: activity} do
+      params = %{
+        "aspect_type" => "create",
+        "object_type" => "activity",
+        "object_id" => activity.external_id,
+        "owner_id" => credential.uid
+      }
+      conn = post(conn, "/webhook/strava", params)
+      assert %Activity{} = Dashboard.get_activity_by_external_id!(credential.user, activity.external_id)
+      assert length(Dashboard.list_activities(credential.user)) == 1
+    end
+
+    test "user deactivates account on strava", %{conn: conn, credential: credential} do
+      params = %{
+        "aspect_type" => "delete",
+        "object_type" => "athlete",
+        "object_id" => credential.uid,
+        "owner_id" => credential.uid
+      }
+      conn = post(conn, "/webhook/strava", params)
+      assert Accounts.get_credential("strava", credential.uid) == nil
+    end
+  end
+
+  defp setup_account(_) do
+    credential = insert(:credential)
+    activity = insert(:activity, user: credential.user)
+
+    {:ok, activity: activity, credential: credential }
   end
 end
