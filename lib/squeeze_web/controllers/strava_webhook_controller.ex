becf72c4c237e88fc396d4036911774c9cfdf8c7
Evan Dancer <evancdancer@gmail.com>
2021-01-01 14:34:43 -0500

    Add strava webhook support for renaming activities
    Also add support for account deletions on strava.

--- lib/squeeze_web/controllers/strava_webhook_controller.ex
+++ lib/squeeze_web/controllers/strava_webhook_controller.ex
@@ -6,6 +6,7 @@ defmodule SqueezeWeb.StravaWebhookController do
   """
 
   alias Squeeze.Accounts
+  alias Squeeze.Dashboard
   alias Squeeze.Challenges.ScoreUpdater
   alias Squeeze.Logger
   alias Squeeze.Strava.ActivityLoader
@@ -15,14 +16,27 @@ defmodule SqueezeWeb.StravaWebhookController do
   plug :validate_token when action in [:challenge]
   plug :log_event
 
-  def webhook(conn, %{"aspect_type" => "create", "object_type" => "activity"} = params) do
-    credential = Accounts.get_credential("strava", params["owner_id"])
-    Task.start(fn ->
-      {:ok, activity} = ActivityLoader.update_or_create_activity(credential, params["object_id"])
-      ScoreUpdater.update_score(activity)
-    end)
+  # User deletes an activity on strava
+  def webhook(conn, %{"aspect_type" => "delete", "object_type" => "activity"} = params) do
+    user = Accounts.get_user_by_credential(%{provider: "strava", uid: params["owner_id"]})
+    activity = Dashboard.get_activity_by_external_id!(user, params["object_id"])
+    Dashboard.delete_activity(activity)
     render(conn, "success.json")
   end
+
+  # User creates or updates an activity on strava
+  def webhook(conn, %{"object_type" => "activity"} = params) do
+    Task.start(fn -> process_event(params) end)
+    render(conn, "success.json")
+  end
+
+  # User deactivates the strava <-> openpace connection
+  def webhook(conn, %{"updates" => %{"authorized" => "false"}, "owner_id" => id}) do
+    credential = Accounts.get_credential("strava", id)
+    Accounts.delete_credential(credential)
+    render(conn, "success.json")
+  end
+
   def webhook(conn, _), do: render(conn, "success.json")
 
   def challenge(conn, %{"hub.challenge" => challenge}) do
@@ -45,6 +59,13 @@ defmodule SqueezeWeb.StravaWebhookController do
     |> put_status(:bad_request)
     |> render("400.json")
   end
+
+  defp process_event(%{"owner_id" => user_id, "object_id" => activity_id}) do
+    credential = Accounts.get_credential("strava", user_id)
+    {:ok, activity} = ActivityLoader.update_or_create_activity(credential, activity_id)
+    ScoreUpdater.update_score(activity)
+  end
+  defp process_event(_), do: nil
 
   defp log_event(conn, _)  do
     body = Jason.encode!(conn.params)
