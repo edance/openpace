a9073250bf88d85794a5fbbb240448402aae1d85
Evan Dancer <evancdancer@gmail.com>
2021-03-24 22:47:56 -0700

    Fix challenge share screen

--- lib/squeeze_web/views/challenge_share_view.ex
+++ lib/squeeze_web/views/challenge_share_view.ex
@@ -9,14 +9,14 @@ defmodule SqueezeWeb.ChallengeShareView do
 
   def remaining_percentage(%{challenge: challenge}) do
     now = DateTime.utc_now()
-    start_at = challenge.start_at
-    end_at = challenge.end_at
+    start_date = challenge.start_date
+    end_date = challenge.end_date
 
     cond do
-      Timex.diff(now, start_at) < 0 -> 0.0
-      Timex.diff(now, end_at) > 0 -> 1 * 100.0
+      Timex.diff(now, start_date) < 0 -> 0.0
+      Timex.diff(now, end_date) > 0 -> 1 * 100.0
       true ->
-        Timex.diff(now, start_at, :seconds) / Timex.diff(end_at, start_at, :seconds) * 100.0
+        Timex.diff(now, start_date, :seconds) / Timex.diff(end_date, start_date, :seconds) * 100.0
     end
   end
 
@@ -29,12 +29,20 @@ defmodule SqueezeWeb.ChallengeShareView do
     end
   end
 
+  def format_score(%{challenge_type: :segment}, amount) do
+    if amount > 0 do
+      format_duration(amount)
+    else
+      "No Attempt"
+    end
+  end
+
   def format_score(challenge, amount) do
     case challenge.challenge_type do
       :distance -> Distances.format(amount)
       :time -> format_duration(amount)
       :altitude -> "#{Distances.to_feet(amount, imperial: true)} ft"
-      :segment -> format_duration(amount)
+      _ -> format_duration(amount)
     end
   end
 end
