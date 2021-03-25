bc06bd199f40c0ac6cc976383b68602afdbdd65b
Evan Dancer <evancdancer@gmail.com>
2021-03-24 22:50:56 -0700

    Remove unused score updater tests

--- test/squeeze/challenges/score_updater_test.exs
+++ test/squeeze/challenges/score_updater_test.exs
@@ -1,32 +1,32 @@
 defmodule Squeeze.Challenges.ScoreUpdaterTest do
   use Squeeze.DataCase
 
-  import Squeeze.Factory
+  # import Squeeze.Factory
 
-  alias Ecto.Changeset
-  alias Squeeze.Challenges
-  alias Squeeze.Challenges.{Challenge, Score}
+  # alias Ecto.Changeset
+  # alias Squeeze.Challenges
+  # alias Squeeze.Challenges.{Challenge, Score}
 
-  describe "#total_amount/2" do
-    test "with segment challenge" do
-    end
+  # describe "#total_amount/2" do
+  #   test "with segment challenge" do
+  #   end
 
-    test "with multiple activities" do
-    end
-  end
+  #   test "with multiple activities" do
+  #   end
+  # end
 
-  describe "#amount/2" do
-    test "with a distance challenge" do
-      insert(:challenge, challenge_type: :distance) |> with_scores(1)
-    end
+  # describe "#amount/2" do
+  #   test "with a distance challenge" do
+  #     insert(:challenge, challenge_type: :distance) |> with_scores(1)
+  #   end
 
-    test "with a time challenge" do
-    end
+  #   test "with a time challenge" do
+  #   end
 
-    test "with an altitude challenge" do
-    end
+  #   test "with an altitude challenge" do
+  #   end
 
-    test "with a segment challenge" do
-    end
-  end
+  #   test "with a segment challenge" do
+  #   end
+  # end
 end
