2b9388ca39283f42bf61cbbe30e59ea9b778b76a
Evan Dancer <evancdancer@gmail.com>
2021-07-29 13:28:29 -0700

    Add tests for social library

--- test/squeeze/social_test.exs
+++ test/squeeze/social_test.exs
@@ -1,62 +1,51 @@
 defmodule Squeeze.SocialTest do
   use Squeeze.DataCase
 
+  alias Squeeze.Accounts
   alias Squeeze.Social
 
-  describe "follows" do
-    alias Squeeze.Social.Follow
+  describe "#list_following/1" do
+    test "includes only followers" do
+      %{follower: user, followee: user2} = insert(:follow)
+      insert(:user)
 
-    @valid_attrs %{}
-    @update_attrs %{}
-    @invalid_attrs %{}
-
-    def follow_fixture(attrs \\ %{}) do
-      {:ok, follow} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Social.create_follow()
-
-      follow
+      list = Social.list_following(user2)
+      assert length(list) == 1
+      assert List.first(list).id == user.id
     end
+  end
 
-    test "list_follows/0 returns all follows" do
-      follow = follow_fixture()
-      assert Social.list_follows() == [follow]
+  describe "#list_following/1" do
+    test "includes only following users" do
+      %{follower: user, followee: user2} = insert(:follow)
+      insert(:user)
+
+      list = Social.list_following(user)
+      assert length(list) == 1
+      assert List.first(list).id == user2.id
     end
+  end
 
-    test "get_follow!/1 returns the follow with given id" do
-      follow = follow_fixture()
-      assert Social.get_follow!(follow.id) == follow
+  describe "#follow_user/2" do
+    test "creates follow record and increments counts" do
+      [user, user2] = insert_pair(:user)
+      Social.follow_user(user, user2)
+
+      assert(Accounts.get_user_by_slug!(user.slug).following_count) == 1
+      assert(Accounts.get_user_by_slug!(user2.slug).followers_count) == 1
+      assert(length(Social.list_following(user))) == 1
     end
+  end
 
-    test "create_follow/1 with valid data creates a follow" do
-      assert {:ok, %Follow{} = follow} = Social.create_follow(@valid_attrs)
-    end
+  describe "#unfollow_user/2" do
+    test "deletes follow record and decrements counts" do
+      [user, user2] = insert_pair(:user, following_count: 1, follower_count: 1)
+      insert(:follow, follower: user, followee: user2)
+      Social.unfollow_user(user, user2)
 
-    test "create_follow/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Social.create_follow(@invalid_attrs)
-    end
-
-    test "update_follow/2 with valid data updates the follow" do
-      follow = follow_fixture()
-      assert {:ok, %Follow{} = follow} = Social.update_follow(follow, @update_attrs)
-    end
-
-    test "update_follow/2 with invalid data returns error changeset" do
-      follow = follow_fixture()
-      assert {:error, %Ecto.Changeset{}} = Social.update_follow(follow, @invalid_attrs)
-      assert follow == Social.get_follow!(follow.id)
-    end
-
-    test "delete_follow/1 deletes the follow" do
-      follow = follow_fixture()
-      assert {:ok, %Follow{}} = Social.delete_follow(follow)
-      assert_raise Ecto.NoResultsError, fn -> Social.get_follow!(follow.id) end
-    end
-
-    test "change_follow/1 returns a follow changeset" do
-      follow = follow_fixture()
-      assert %Ecto.Changeset{} = Social.change_follow(follow)
+      assert(Accounts.get_user_by_slug!(user.slug).following_count) == 0
+      assert(Accounts.get_user_by_slug!(user2.slug).followers_count) == 0
+      assert(Social.list_following(user)) == []
     end
   end
 end
