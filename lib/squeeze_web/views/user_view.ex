26b6ec0795bb6f23cf0b6fb7ee367201ed7cb55d
Evan Dancer <evancdancer@gmail.com>
2021-11-10 10:50:03 -0800

    Fix signup page

--- lib/squeeze_web/views/user_view.ex
+++ lib/squeeze_web/views/user_view.ex
@@ -1,6 +1,10 @@
 defmodule SqueezeWeb.UserView do
   use SqueezeWeb, :view
 
+  def title("new.html", _) do
+    "Create an Account"
+  end
+
   def title(_page, _assigns) do
     gettext("User")
   end
