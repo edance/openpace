26cd5a0a950b98ebccbbcb525d98c2b94938968e
Evan Dancer <evancdancer@gmail.com>
2021-11-11 20:18:53 -0800

    Fix webpack config

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,60 +1,66 @@
 const path = require('path');
 const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = (env, options) => ({
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  },
-  entry: {
-    'app': './js/app.js',
-    'dashboard': './js/dashboard.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '../priv/static/js')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: require.resolve('jquery'),
-        use: [{
-          loader: 'expose-loader',
-          options: 'jQuery'
-        },{
-          loader: 'expose-loader',
-          options: '$'
-        }]
-      },
-      {
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
-        loader: 'url-loader?limit=30000'
-      }
-    ]
-  },
-  plugins: [
+module.exports = (env, options) => {
+  const plugins = [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-  ]
-});
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
+  ].concat(process.argv.includes("--withReport")? [new BundleAnalyzerPlugin()] : []);
+
+  return {
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new OptimizeCSSAssetsPlugin({}),
+        new TerserPlugin(),
+      ]
+    },
+    entry: {
+      'app': './js/app.js',
+      'dashboard': './js/dashboard.js',
+    },
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, '../priv/static/js')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+          ]
+        },
+        {
+          test: require.resolve('jquery'),
+          use: [{
+            loader: 'expose-loader',
+            options: 'jQuery'
+          },{
+            loader: 'expose-loader',
+            options: '$'
+          }]
+        },
+        {
+          test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+          loader: 'url-loader?limit=30000'
+        }
+      ]
+    },
+    plugins,
+  }
+};
