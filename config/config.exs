321dabe46a4d95f0aa8cde8d63280860ee467632
Evan Dancer <evancdancer@gmail.com>
2021-11-17 16:42:29 -0800

    Update to phx 1.5

--- config/config.exs
+++ config/config.exs
@@ -16,8 +16,7 @@ config :squeeze, SqueezeWeb.Endpoint,
   url: [scheme: "http", host: "localhost", port: 4000],
   secret_key_base: "dQoFqEeDanLKBUOlYj3JJN3GOKv3AK9id6Je17YxqP2wg+c1Bs8ckUPo9YDTPD1k",
   render_errors: [view: SqueezeWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Squeeze.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub_server: Squeeze.PubSub
 
 # Configures Elixir's Logger
 config :logger, :console,
