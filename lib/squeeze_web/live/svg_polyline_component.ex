065117f554a1d26df60b6c2fe5bf6cf21e2fc562
Evan Dancer <evancdancer@gmail.com>
2021-12-04 12:47:15 -0800

    Add components for static map and svg maps

---
+++ lib/squeeze_web/live/svg_polyline_component.ex
@@ -0,0 +1,34 @@
+defmodule SqueezeWeb.SvgPolylineComponent do
+  use SqueezeWeb, :live_component
+
+  # Converted from JS here: https://stackoverflow.com/questions/46646901/convert-google-maps-polyline-to-svg
+
+  def svg_path(nil), do: nil
+  def svg_path(polyline) do
+    require IEx; IEx.pry
+    coords = Polyline.decode(polyline)
+    points = coords |> Enum.map(&coord_to_point/1)
+    min_x = points |> Enum.map(&(&1.x)) |> Enum.min()
+    min_y = points |> Enum.map(&(&1.y)) |> Enum.min()
+    max_x = points |> Enum.map(&(&1.x)) |> Enum.max()
+    max_y = points |> Enum.map(&(&1.y)) |> Enum.max()
+    svg_path = points |> Enum.map(&("#{&1.x},#{&1.y}")) |> Enum.join(" ")
+    stroke_width = Enum.max([(max_x - min_x) * 0.01, (max_y - min_y) * 0.01])
+
+    %{
+      path: "M#{svg_path}",
+      x: min_x - stroke_width,
+      y: min_y - stroke_width,
+      width: max_x - min_x + stroke_width * 2,
+      height: max_y - min_y + stroke_width * 2,
+      stroke_width: stroke_width
+    }
+  end
+
+  def coord_to_point({lon, lat}) do
+    %{
+      x: (lon + 180) * (256 / 360),
+      y: (256 / 2) - (256 * :math.log(:math.tan((:math.pi() / 4) + ((lat * :math.pi() / 180) / 2))) / (2 * :math.pi()))
+    }
+  end
+end
---
+++ lib/squeeze_web/utils/mapbox_static_map.ex
@@ -0,0 +1,121 @@
+defmodule SqueezeWeb.MapboxStaticMap do
+  # Converted from JS here: https://blog.mapbox.com/generate-gradient-lines-with-the-static-image-api-368eb28068a3
+
+  @token Application.get_env(:squeeze, :mapbox_access_token)
+
+  @doc """
+  Get a mapbox static url for a polyline
+
+  ## Options
+  * height (integer): height of the image
+  * width (integer): width of the image
+  * show_pins (boolean): show or hide the start and end markers
+  * start_color (string): start of the gradient color in hex (example "#FF512F")
+  * end_color (string): end of the gradient color in hex (example "#FF512F")
+  * stroke_width (integer): width of the path
+  """
+  def map_url(polyline, opts \\ []) do
+    height = Keyword.get(opts, :height, 256)
+    width = Keyword.get(opts, :height, 256)
+    show_pins = Keyword.get(opts, :show_pins, false)
+
+    encoded_path = URI.encode(make_path(polyline, opts), &URI.char_unreserved?(&1))
+
+    "https://api.mapbox.com/styles/v1/mapbox/light-v9/static/#{encoded_path}/auto/#{width}x#{height}@2x?access_token=#{@token}"
+  end
+
+  defp make_path(polyline, opts \\ []) do
+    show_pins = Keyword.get(opts, :show_pins, false)
+    coords = decode_polyline(polyline)
+
+    if show_pins do
+      "#{make_path_with_gradient(coords, opts)},#{make_pins(coords, opts)}"
+    else
+      make_path_with_gradient(coords, opts)
+    end
+  end
+
+  defp make_path_with_gradient(coords, opts \\ []) do
+    start_color = Keyword.get(opts, :start_color, "#FF512F")
+    end_color = Keyword.get(opts, :end_color, "#F09819")
+    stroke_width = Keyword.get(opts, :stroke_width, 2)
+
+    color_a = hex_str_to_rgb(start_color)
+    color_b = hex_str_to_rgb(end_color)
+    spectrum_colors = create_spectrum(color_a, color_b, length(coords) - 1)
+
+    [first_coord | coords] = coords
+    {paths, _} = Enum.map_reduce(coords, first_coord, fn c2, c1 -> {[c1, c2], c2} end)
+
+    # format from https://docs.mapbox.com/api/maps/#path
+    paths
+    |> Enum.zip(spectrum_colors)
+    |> Enum.map(fn {path, color} -> "path-#{stroke_width}+#{color}(#{Polyline.encode(path)})" end)
+    |> Enum.join(",")
+  end
+
+  defp make_pins(coords, opts \\ []) do
+    start_color = Keyword.get(opts, :start_color, "#FF512F")
+    end_color = Keyword.get(opts, :end_color, "#F09819")
+    color_a = hex_str_to_rgb(start_color)
+    color_b = hex_str_to_rgb(end_color)
+    {lon1, lat1} = List.first(coords)
+    {lon2, lat2} = List.last(coords)
+
+    Enum.join([
+      "pin-s-a+#{rgb_to_hex_str(color_a)}(#{lon1},#{lat1})",
+      "pin-s-b+#{rgb_to_hex_str(color_b)}(#{lon2},#{lat2})"
+    ], ",")
+  end
+
+  defp decode_polyline(polyline) do
+    coords = Polyline.decode(polyline)
+    length = length(coords)
+
+    if length > 100 do
+      Enum.take_every(coords, :math.ceil(length / 100))
+    else
+      coords
+    end
+  end
+
+  defp dec_to_hex(dec) do
+    pad = if dec < 16 do
+      "0"
+    else
+      ""
+    end
+    "#{pad}#{Integer.to_string(dec, 16)}"
+  end
+
+  defp hex_to_dec(hex) do
+    {x, ""} = Integer.parse(hex, 16)
+    x
+  end
+
+  defp rgb_to_hex_str(rgb) do
+    rgb
+    |> Enum.map(&dec_to_hex/1)
+    |> Enum.join("")
+  end
+
+  defp hex_str_to_rgb(hex_str) do
+    hex_str
+    |> String.replace("#", "")
+    |> String.graphemes()
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn ([a, b])  -> hex_to_dec("#{a}#{b}") end)
+  end
+
+  defp create_spectrum(start_rgb, end_rgb, steps) do
+    [s_red, s_green, s_blue] = start_rgb
+    [e_red, e_green, e_blue] = end_rgb
+
+    Enum.map(0..steps, fn i ->
+      r = round(((e_red - s_red) * i) / steps) + s_red
+      g = round(((e_green - s_green) * i) / steps) + s_green
+      b = round(((e_blue - s_blue) * i) / steps) + s_blue
+      rgb_to_hex_str([r, g, b])
+    end)
+  end
+end
