48a542d6b2c95afc277ad6bec401f8b9b0ed77c0
Evan Dancer <evancdancer@gmail.com>
2021-12-27 12:00:05 -0500

    Fix authentication for live views in dashboard

--- lib/squeeze/auth_error_handler.ex
+++ lib/squeeze/auth_error_handler.ex
@@ -6,11 +6,18 @@ defmodule Squeeze.AuthErrorHandler do
   import Plug.Conn
 
   alias Phoenix.Controller
+  alias SqueezeWeb.Router.Helpers, as: Routes
 
-  def auth_error(conn, _, _opts) do
+  def auth_error(conn, {:unauthenticated, _reason}, _opts) do
     conn
     |> clear_session()
-    |> Controller.redirect(to: "/")
+    |> Controller.put_flash(:error, "You must be logged in to view")
+    |> Controller.redirect(to: Routes.session_path(conn, :new))
+    |> halt()
+  end
+  def auth_error(conn, {:already_authenticated, _reason}, _opts) do
+    conn
+    |> Controller.redirect(to: Routes.overview_path(conn, :index))
     |> halt()
   end
 end
---
+++ lib/squeeze/live_auth_pipeline.ex
@@ -0,0 +1,13 @@
+defmodule Squeeze.LiveAuthPipeline do
+  @moduledoc """
+  This module defines the pipeline for live view auth allowing a shared error handler
+  across all plugs.
+  """
+
+  use Guardian.Plug.Pipeline, otp_app: :squeeze,
+    module: Squeeze.Guardian,
+    error_handler: Squeeze.AuthErrorHandler
+
+  plug Guardian.Plug.VerifySession
+  plug Guardian.Plug.EnsureAuthenticated
+end
