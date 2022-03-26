b9cd8f95e82d360db351f765fa436306dfa9ad72
Evan Dancer <evancdancer@gmail.com>
2022-03-26 10:21:35 -0700

    Add FAQ to namer page

--- lib/squeeze_web/views/home_view.ex
+++ lib/squeeze_web/views/home_view.ex
@@ -1,5 +1,6 @@
 defmodule SqueezeWeb.HomeView do
   use SqueezeWeb, :view
 
+  def title("namer.html", _), do: gettext("Rename your Strava Activities Automatically")
   def title(_, _), do: gettext("Run Your Best Race Ever")
 end
