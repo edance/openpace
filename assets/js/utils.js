5d6089fd980834b622e581db29e55d8e3a43c5e2
Evan Dancer <evancdancer@gmail.com>
2022-07-05 09:41:28 -0700

    Add more chartjs graphs

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -78,3 +78,15 @@ export function getFullMonths() {
     'December'
   ];
 }
+
+export function hexToRGB(hex, alpha) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  if (alpha) {
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  } else {
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+}
