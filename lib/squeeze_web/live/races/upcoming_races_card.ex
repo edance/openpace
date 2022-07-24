5d2ecad52e189d1a198db7c87c1ae702ce54ea35
Evan Dancer <evancdancer@gmail.com>
2022-07-23 20:57:16 -0700

    Add features to upcoming races card

--- lib/squeeze_web/live/races/upcoming_races_card.ex
+++ lib/squeeze_web/live/races/upcoming_races_card.ex
@@ -1,8 +1,32 @@
 defmodule SqueezeWeb.Races.UpcomingRacesCard do
   use SqueezeWeb, :live_component
 
+  alias Squeeze.Distances
+
+  @colors ~w(
+    blue
+    indigo
+    purple
+    red
+    orange
+    yellow
+    green
+    teal
+    cyan
+  )
+
   def race_date(%{start_date: date}) do
     date
     |> Timex.format!("%B #{Ordinal.ordinalize(date.day)}, %Y", :strftime)
+  end
+
+  def bg_color(model) do
+    idx = rem(model.id, length(@colors))
+    color = Enum.at(@colors, idx)
+    "bg-gradient-#{color}"
+  end
+
+  def distance_name(race, current_user) do
+    Distances.distance_name(race.distance, imperial: current_user.user_prefs.imperial)
   end
 end
