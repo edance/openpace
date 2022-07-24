4a4a9ae3a76cc49d16db4bad7d2bcb9036832fd2
Evan Dancer <evancdancer@gmail.com>
2022-07-24 08:05:50 -0700

    Add wave background to all dashboard pages

--- lib/squeeze_web/live/challenges/show_live.ex
+++ lib/squeeze_web/live/challenges/show_live.ex
@@ -5,10 +5,11 @@ defmodule SqueezeWeb.Challenges.ShowLive do
 
   @impl true
   def mount(%{"id" => slug}, session, socket) do
+    user = socket.assigns[:current_user] || get_current_user(session)
     challenge = Challenges.get_challenge_by_slug!(slug)
 
     socket = socket
-    |> assign_new(:current_user, fn -> get_current_user(session) end)
+    |> assign(:current_user, user)
     |> assign(challenge: challenge)
 
     {:ok, socket}
