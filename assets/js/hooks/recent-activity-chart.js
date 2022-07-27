8b2f2bb004d3694c0ddd8f1bab14667d71564965
Evan Dancer <evancdancer@gmail.com>
2022-07-27 15:27:50 -0700

    Add ability to toggle recent activity chart

--- assets/js/hooks/recent-activity-chart.js
+++ assets/js/hooks/recent-activity-chart.js
@@ -1,8 +1,8 @@
 import Chart from 'chart.js/auto';
 import { parse } from 'date-fns';
 import { colors, fonts } from './../variables.js';
-import { activityColor, calcDistance, calcFeet, hexToRGB, roundTo } from '../utils';
-import { DateTime } from "luxon";
+import { activityColor, calcDistance, calcFeet, formatNumber, hexToRGB, roundTo } from '../utils';
+import { DateTime, Duration } from "luxon";
 
 function lastXWeeks() {
   const weekCount = 52;
@@ -26,63 +26,97 @@ function eligibleTypes(field, activities) {
   return Object.keys(sumByType).filter((x) => sumByType[x] > 0);
 }
 
-function weeklyAmountTotalByType(type, field, activities) {
-  const weeks = lastXWeeks();
-  const dates = weeks.reduce((obj, week) => {
-    obj[week.toISODate()] = 0;
-    return obj;
-  }, {});
+export default {
+  weeklyAmountTotalByType(type) {
+    const dates = this.weeks.reduce((obj, week) => {
+      obj[week.toISODate()] = 0;
+      return obj;
+    }, {});
 
-  const dateMap = activities
-    .filter(x => x.type === type)
-    .reduce(function(obj, activity){
-      const date = DateTime.fromISO(activity.start_at_local).startOf("week");
-      const dateStr = date.toISODate();
-      if (Object.keys(obj).includes(dateStr)) {
-        obj[dateStr] = obj[dateStr] + activity[field] / 1609;
+    const dateMap = this.activities
+          .filter(x => x.type === type)
+          .reduce((obj, activity) => {
+            const date = DateTime.fromISO(activity.start_at_local).startOf("week");
+            const dateStr = date.toISODate();
+
+            if (Object.keys(obj).includes(dateStr)) {
+              obj[dateStr] = obj[dateStr] + activity[this.field];
+            }
+
+            return obj;
+          }, dates);
+
+    return Object.keys(dateMap).sort().map(key => {
+      if (this.field === 'distance') {
+        return calcDistance(dateMap[key], this.imperial, 1);
+      }
+      if (this.field === 'elevation_gain') {
+        return calcFeet(dateMap[key], this.imperial, 0);
       }
 
-      return obj;
-    }, dates);
-
-  return Object.keys(dateMap).sort().map(key => dateMap[key]);
-}
-
-
-export default {
-  mounted() {
-    const activities = JSON.parse(this.el.dataset['summaries']);
-    const imperial = JSON.parse(this.el.dataset['imperial']);
-    const weeks = lastXWeeks();
-    const field = 'distance';
-    const types = eligibleTypes(field, activities);
-
+      return dateMap[key];
+    });
+  },
+  calcChartData() {
+    const types = eligibleTypes(this.field, this.activities);
     const datasets = types.map(type => {
       return {
         label: type,
         backgroundColor: hexToRGB(activityColor(type), 0.2),
         borderColor: hexToRGB(activityColor(type), 1.0),
         borderWidth: 1,
-        data: weeklyAmountTotalByType(type, field, activities),
+        data: this.weeklyAmountTotalByType(type, this.field, this.activities),
       };
     });
 
-    const data = {
+    return {
       datasets,
-      labels: weeks.map(x => x.toLocaleString({ month: 'short', day: 'numeric' })),
+      labels: this.weeks.map(x => x.toLocaleString({ month: 'short', day: 'numeric' })),
     };
+  },
+
+  mounted() {
+    this.activities = JSON.parse(this.el.dataset['summaries']);
+    this.imperial = JSON.parse(this.el.dataset['imperial']);
+    this.weeks = lastXWeeks();
+    this.field = 'distance';
 
     const hook = this;
     const ctx = this.el.getContext('2d');
     this.chart = new Chart(ctx, {
       type: 'bar',
-      data,
+      data: this.calcChartData(),
       options: {
         aspectRatio: 4 / 2,
         elements: {
           pointStyle: 'circle',
         },
         plugins: {
+          tooltip: {
+            callbacks: {
+              title: (context) => {
+                if (context.length !== 1) {
+                  return null;
+                }
+
+                return `Week of ${context[0].label}`;
+              },
+              label: (context) => {
+                if (this.field === 'distance') {
+                  const label = this.imperial ? 'mi' : 'km';
+                  return `${context.dataset.label}: ${context.formattedValue} ${label}`;
+                }
+                if (this.field === 'elevation_gain') {
+                  const label = this.imperial ? 'ft' : 'm';
+                  return `${context.dataset.label}: ${context.formattedValue} ${label}`;
+                }
+
+                const value = context.dataset.data[context.dataIndex];
+                const duration = Duration.fromObject({ seconds: value });
+                return `${context.dataset.label}: ${duration.toFormat("h 'hr' mm 'min'")}`;
+              },
+            },
+          },
           legend: {
             position: 'bottom',
             labels: {
@@ -110,6 +144,19 @@ export default {
             },
             ticks: {
               color: 'white',
+              callback: (value, index, ticks) => {
+                if (this.field === 'distance') {
+                  const label = this.imperial ? 'mi' : 'km';
+                  return formatNumber(value) + label;
+                }
+                if (this.field === 'elevation_gain') {
+                  const label = this.imperial ? 'ft' : 'm';
+                  return formatNumber(value) + label;
+                }
+
+                const duration = Duration.fromObject({ seconds: value });
+                return duration.toFormat("h'hr'");
+              },
             },
           },
         },
@@ -130,12 +177,20 @@ export default {
 
           if (points.length) {
             const firstPoint = points[0];
-            const date = weeks[firstPoint.index];
+            const date = this.weeks[firstPoint.index];
             // Send event to backend
             hook.pushEvent("load-week", { date: date.toISODate() });
           }
         }
       },
+    });
+
+    $('#recent-activities-chart-type').on('change', (evt) => {
+      this.field = evt.currentTarget.value;
+      this.chart.data = this.calcChartData();
+      this.chart.options.scales.y.ticks['stepSize'] = this.field === 'duration' ? 3600 : null;
+
+      this.chart.update();
     });
   }
 }
