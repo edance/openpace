7d3c7f155ac3e9d399137617126bd456c5062856
Evan Dancer <evancdancer@gmail.com>
2022-08-05 13:34:35 -0700

    Add ability to save personal records

--- lib/squeeze_web/live/settings/personal_records_form_component.ex
+++ lib/squeeze_web/live/settings/personal_records_form_component.ex
@@ -3,6 +3,9 @@ defmodule SqueezeWeb.Settings.PersonalRecordsFormComponent do
   """
 
   use SqueezeWeb, :live_component
+
+  alias Phoenix.LiveView.JS
+
   import Squeeze.Distances, only: [distance_name: 2]
 
   def pr_at_distance(%{current_user: user}, distance) do
