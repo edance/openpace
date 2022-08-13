9ac160f2f445c36b59abaf3818ec7c49d906c785
Evan Dancer <evancdancer@gmail.com>
2022-08-12 21:26:19 -0700

    Fix races show for just finish race goals

--- lib/squeeze_web/live/races/show_live.ex
+++ lib/squeeze_web/live/races/show_live.ex
@@ -13,14 +13,14 @@ defmodule SqueezeWeb.Races.ShowLive do
   def mount(%{"slug" => slug}, session, socket) do
     user = socket.assigns[:current_user] || get_current_user(session)
     race_goal = Races.get_race_goal!(slug)
-    vo2_max = RacePredictor.estimated_vo2max(race_goal.distance, race_goal.duration)
+    vo2_max = vo2_max(race_goal)
 
     socket = assign(socket,
       page_title: race_goal.race.name,
       race_goal: race_goal,
       race: race_goal.race,
       paces: race_goal.training_paces,
-      predictions: RacePredictor.predict_all_race_times(vo2_max),
+      predictions: predictions(vo2_max),
       vo2_max: vo2_max,
       current_user: user
     )
@@ -40,5 +40,15 @@ defmodule SqueezeWeb.Races.ShowLive do
 
   def distance_name(distance, current_user) do
     Distances.distance_name(distance, imperial: current_user.user_prefs.imperial)
+  end
+
+  defp vo2_max(%{duration: nil}), do: nil
+  defp vo2_max(%{distance: distance, duration: duration}) do
+    RacePredictor.estimated_vo2max(distance, duration)
+  end
+
+  defp predictions(nil), do: nil
+  defp predictions(vo2_max) do
+    RacePredictor.predict_all_race_times(vo2_max)
   end
 end
