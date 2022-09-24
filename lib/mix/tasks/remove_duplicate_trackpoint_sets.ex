d2c5dc3abcfc1140c7975b3b3b135cf1a115a205
Evan Dancer <evancdancer@gmail.com>
2022-09-23 20:15:37 -0700

    Add task to remove duplicate trackpoint sets

---
+++ lib/mix/tasks/remove_duplicate_trackpoint_sets.ex
@@ -0,0 +1,28 @@
+defmodule Mix.Tasks.RemoveDuplicateTrackpointSets do
+  use Mix.Task
+  @moduledoc false
+
+  import Ecto.Query, warn: false
+  alias Squeeze.Dashboard.TrackpointSet
+  alias Squeeze.Repo
+
+  @doc false
+  def run(_) do
+    Mix.Task.run("app.start")
+
+    subquery = from ts in TrackpointSet,
+      select: %{id: ts.id, row_number: row_number() |> over(:partition)},
+      windows: [partition: [partition_by: :activity_id, order_by: :id]]
+
+    query = from ts in TrackpointSet,
+      join: s in subquery(subquery),
+      on: ts.id == s.id and s.row_number > 1,
+      select: ts.id
+
+    Repo.all(query)
+    |> Enum.chunk_every(1000)
+    |> Enum.map(fn chunk ->
+      Repo.delete_all(from ts in TrackpointSet, where: ts.id in ^chunk)
+    end)
+  end
+end
