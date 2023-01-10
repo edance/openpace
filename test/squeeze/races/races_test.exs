4a80a035a0b392831ffdb02437e50ead23c6b4c6
Evan Dancer <evancdancer@gmail.com>
2023-01-09 20:30:48 -0800

    Fix race goal create
    Race was missing the required column of slug. Added the tests as well.

--- test/squeeze/races/races_test.exs
+++ test/squeeze/races/races_test.exs
@@ -12,4 +12,32 @@ defmodule Squeeze.RacesTest do
       assert Races.get_race!(slug).slug == slug
     end
   end
+
+  describe "race_goals" do
+    test "get_race_goal!/1 returns the race with given slug" do
+      race_goal = insert(:race_goal)
+      slug = race_goal.slug
+      assert Races.get_race_goal!(slug).slug == slug
+    end
+
+    test "create_race_goal/2 with valid attrs" do
+      user = insert(:user)
+      race_attrs = params_for(:race)
+      attrs = params_for(:race_goal)
+      attrs = Map.merge(attrs, %{race: race_attrs})
+
+      {:ok, race_goal} = Races.create_race_goal(user, attrs)
+      assert race_goal.slug
+      assert race_goal.race_id
+    end
+
+    test "create_race_goal/2 with invalid attrs" do
+      user = insert(:user)
+      race_attrs = params_for(:race)
+      attrs = params_for(:race_goal)
+      attrs = Map.merge(attrs, %{distance: nil, race: race_attrs})
+
+      assert {:error, _} = Races.create_race_goal(user, attrs)
+    end
+  end
 end
