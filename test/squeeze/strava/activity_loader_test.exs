23ded5a4997fe539f27a9013106e65df5951ed08
Evan Dancer <evancdancer@gmail.com>
2023-01-16 20:23:07 -0800

    Fix errors when strava activity or stream is not found

--- test/squeeze/strava/activity_loader_test.exs
+++ test/squeeze/strava/activity_loader_test.exs
@@ -8,19 +8,42 @@ defmodule Squeeze.ActivityLoaderTest do
   alias Squeeze.TimeHelper
 
   describe "update_or_create_activity/2" do
-    setup [:build_strava_activity, :setup_mocks, :create_credential]
+    setup [:build_strava_activity, :create_credential]
 
-    test "creates an activity if none exist",
-      %{credential: credential, activity: strava_activity} do
+    test "creates an activity if none exist", context do
+      %{credential: credential, activity: strava_activity} = context
+      setup_mocks(context)
       assert {:ok, _} = ActivityLoader.update_or_create_activity(credential, strava_activity.id)
     end
 
-    test "updates activity if matched activity exists",
-      %{credential: credential, activity: strava_activity} do
+    test "updates activity if matched activity exists", context do
+      %{credential: credential, activity: strava_activity} = context
+      setup_mocks(context)
       distance = strava_activity.distance
       user = credential.user
       insert(:activity, user: user, planned_distance: distance, planned_date: TimeHelper.today(user))
       assert {:ok, _} = ActivityLoader.update_or_create_activity(credential, strava_activity.id)
+    end
+
+    test "works with activities without streams", context do
+      %{credential: credential, activity: strava_activity} = context
+      mock_client(context)
+      mock_activities(context)
+
+      Squeeze.Strava.MockStreams
+      |> expect(:get_activity_streams, fn(_, _, _, _) -> {:error, %{status: 404}} end)
+
+      assert {:ok, _} = ActivityLoader.update_or_create_activity(credential, strava_activity.id)
+    end
+
+    test "returns error if activity is not found", context do
+      %{credential: credential, activity: strava_activity} = context
+      mock_client(context)
+
+      Squeeze.Strava.MockActivities
+      |> expect(:get_activity_by_id, fn(_, _) -> {:error, %{status: 404}} end)
+
+      assert {:error, _} = ActivityLoader.update_or_create_activity(credential, strava_activity.id)
     end
   end
 
@@ -28,12 +51,19 @@ defmodule Squeeze.ActivityLoaderTest do
     {:ok, activity: build(:detailed_activity)}
   end
 
-  defp setup_mocks(%{activity: activity}) do
+  defp mock_client(_)  do
     Squeeze.Strava.MockClient
     |> expect(:new, 2, fn(_, _) -> %Tesla.Client{} end)
+  end
 
+  defp mock_activities(%{activity: activity}) do
     Squeeze.Strava.MockActivities
     |> expect(:get_activity_by_id, fn(_, _) -> {:ok, activity} end)
+  end
+
+  defp setup_mocks(context) do
+    mock_client(context)
+    mock_activities(context)
 
     Squeeze.Strava.MockStreams
     |> expect(:get_activity_streams, fn(_, _, _, _) -> {:ok, %Strava.StreamSet{}} end)
