dc9478737b9b4081c7aab0b69ef84bed17da76bc
Evan Dancer <evancdancer@gmail.com>
2023-01-25 20:39:40 -0800

    Fix height for mobile recent activities chart

--- assets/js/hooks/recent-activity-chart.js
+++ assets/js/hooks/recent-activity-chart.js
@@ -93,7 +93,7 @@ export default {
       type: 'bar',
       data: this.calcChartData(),
       options: {
-        aspectRatio: 4 / 2,
+        maintainAspectRatio: false,
         elements: {
           pointStyle: 'circle',
         },
