05de374ecf30385e197ee17146ecfdc6d296ff12
Evan Dancer <evancdancer@gmail.com>
2023-02-06 20:55:19 -0800

    Fix app.js and dashboard.js hooks into different bundles

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,4 +16,27 @@ import './components/base';
 import './fonts';
 
 // Live View
-import './live_view';
+import {Socket} from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+
+// Import only hooks not used on the dashboard
+import hooks from './hooks/base';
+
+const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+const liveSocket = new LiveSocket("/live", Socket, {
+  hooks,
+  params: {_csrf_token: csrfToken}
+});
+
+// Connect if there are any LiveViews on the page
+liveSocket.connect();
+
+// Expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)
+// The latency simulator is enabled for the duration of the browser session.
+// Call disableLatencySim() to disable:
+// >> liveSocket.disableLatencySim()
+liveSocket.enableDebug();
+
+window.liveSocket = liveSocket;
---
+++ assets/js/hooks/dashboard.js
@@ -0,0 +1,19 @@
+import ActivityBubbleChart from "./activity-bubble-chart";
+import ActivityMap from "./activity-map";
+import CalendarChart from "./calendar-chart";
+import DurationSelect from "./duration-select";
+import Modal from "./modal";
+import RecentActivityChart from "./recent-activity-chart";
+import SlimSelect from "./slim-select";
+import TrendCard from "./trend-card";
+
+export default {
+  ActivityBubbleChart,
+  ActivityMap,
+  CalendarChart,
+  DurationSelect,
+  Modal,
+  RecentActivityChart,
+  SlimSelect,
+  TrendCard,
+};
