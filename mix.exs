38a41254891570a15c6345df4dceb23add61c16a
Evan Dancer <evancdancer@gmail.com>
2023-02-15 20:30:09 -0800

    Bump elixir version to 1.14.2 for ml libraries

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Squeeze.Mixfile do
     [
       app: :squeeze,
       version: "0.0.1",
-      elixir: "~> 1.14",
+      elixir: "~> 1.14.2",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
