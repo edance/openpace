55fa92b7d6301d033cca8602b67d4d69f415c238
Evan Dancer <evancdancer@gmail.com>
2023-02-22 20:53:04 -0800

    Add gender and birthdate to settings page

--- lib/squeeze_web/live/settings/user_form_component.ex
+++ lib/squeeze_web/live/settings/user_form_component.ex
@@ -2,10 +2,24 @@ defmodule SqueezeWeb.Settings.UserFormComponent do
   use SqueezeWeb, :live_component
   @moduledoc false
 
-  def time_zones do
+  alias Squeeze.Accounts.UserPrefs
+
+  defp time_zones do
     Tzdata.zone_list()
-    |> Enum.map(fn(x) ->
+    |> Enum.map(fn x ->
       {String.replace(x, "_", " "), x}
     end)
+  end
+
+  defp gender_opts do
+    Ecto.Enum.mappings(UserPrefs, :gender)
+    |> Enum.map(fn {k, _} -> {format_option(k), k} end)
+  end
+
+  defp format_option(opt) do
+    opt
+    |> Atom.to_string()
+    |> String.replace("_", " ")
+    |> String.capitalize()
   end
 end
