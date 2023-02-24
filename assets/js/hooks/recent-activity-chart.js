c398db1e9de9c6a2c32d1c0d6c4d44fd55672f11
Evan Dancer <evancdancer@gmail.com>
2023-02-24 13:43:50 -0800

    Fix overview chart tooltips to show activity_type

--- assets/js/hooks/recent-activity-chart.js
+++ assets/js/hooks/recent-activity-chart.js
@@ -1,6 +1,14 @@
-import Chart from 'chart.js/auto';
-import { colors, fonts } from './../variables.js';
-import { activityColor, calcDistance, calcFeet, formatNumber, hexToRGB, roundTo } from '../utils';
+import Chart from "chart.js/auto";
+import { colors, fonts } from "./../variables.js";
+import {
+  activityColor,
+  calcDistance,
+  calcFeet,
+  formatNumber,
+  hexToRGB,
+  roundTo,
+  capitalize,
+} from "../utils";
 import { DateTime, Duration } from "luxon";
 
 function lastXWeeks() {
@@ -33,37 +41,39 @@ export default {
     }, {});
 
     const dateMap = this.activities
-          .filter(x => x.activity_type === type)
-          .reduce((obj, activity) => {
-            const date = DateTime.fromISO(activity.start_at_local).startOf("week");
-            const dateStr = date.toISODate();
+      .filter((x) => x.activity_type === type)
+      .reduce((obj, activity) => {
+        const date = DateTime.fromISO(activity.start_at_local).startOf("week");
+        const dateStr = date.toISODate();
 
-            if (Object.keys(obj).includes(dateStr)) {
-              obj[dateStr] = obj[dateStr] + activity[this.field];
-            }
+        if (Object.keys(obj).includes(dateStr)) {
+          obj[dateStr] = obj[dateStr] + activity[this.field];
+        }
 
-            return obj;
-          }, dates);
+        return obj;
+      }, dates);
 
-    return Object.keys(dateMap).sort().map(key => {
-      if (this.field === 'distance') {
-        return calcDistance(dateMap[key], this.imperial, 1);
-      }
-      if (this.field === 'elevation_gain') {
-        return calcFeet(dateMap[key], this.imperial, 0);
-      }
+    return Object.keys(dateMap)
+      .sort()
+      .map((key) => {
+        if (this.field === "distance") {
+          return calcDistance(dateMap[key], this.imperial, 1);
+        }
+        if (this.field === "elevation_gain") {
+          return calcFeet(dateMap[key], this.imperial, 0);
+        }
 
-      return dateMap[key];
-    });
+        return dateMap[key];
+      });
   },
   calcChartData() {
     const types = eligibleTypes(this.field, this.activities);
-    const datasets = types.map(type => {
+    const datasets = types.map((type) => {
       return {
         label: type,
         backgroundColor: (context) => {
           const week = this.weeks[context.dataIndex];
-          if (this.currentDate.hasSame(week, 'week')) {
+          if (this.currentDate.hasSame(week, "week")) {
             return hexToRGB(activityColor(type), 0.8);
           }
           return hexToRGB(activityColor(type), 0.2);
@@ -76,26 +86,28 @@ export default {
 
     return {
       datasets,
-      labels: this.weeks.map(x => x.toLocaleString({ month: 'short', day: 'numeric' })),
+      labels: this.weeks.map((x) =>
+        x.toLocaleString({ month: "short", day: "numeric" })
+      ),
     };
   },
 
   mounted() {
-    this.activities = JSON.parse(this.el.dataset['summaries']);
-    this.imperial = JSON.parse(this.el.dataset['imperial']);
-    this.currentDate = DateTime.fromISO(this.el.dataset['currentDate']);
+    this.activities = JSON.parse(this.el.dataset["summaries"]);
+    this.imperial = JSON.parse(this.el.dataset["imperial"]);
+    this.currentDate = DateTime.fromISO(this.el.dataset["currentDate"]);
     this.weeks = lastXWeeks();
-    this.field = 'distance';
+    this.field = "distance";
 
     const hook = this;
-    const ctx = this.el.getContext('2d');
+    const ctx = this.el.getContext("2d");
     this.chart = new Chart(ctx, {
-      type: 'bar',
+      type: "bar",
       data: this.calcChartData(),
       options: {
         maintainAspectRatio: false,
         elements: {
-          pointStyle: 'circle',
+          pointStyle: "circle",
         },
         plugins: {
           tooltip: {
@@ -108,27 +120,29 @@ export default {
                 return `Week of ${context[0].label}`;
               },
               label: (context) => {
-                if (this.field === 'distance') {
-                  const label = this.imperial ? 'mi' : 'km';
-                  return `${context.dataset.label}: ${context.formattedValue} ${label}`;
+                const type = capitalize(context.dataset.label);
+
+                if (this.field === "distance") {
+                  const label = this.imperial ? "mi" : "km";
+                  return `${type}: ${context.formattedValue} ${label}`;
                 }
-                if (this.field === 'elevation_gain') {
-                  const label = this.imperial ? 'ft' : 'm';
-                  return `${context.dataset.label}: ${context.formattedValue} ${label}`;
+                if (this.field === "elevation_gain") {
+                  const label = this.imperial ? "ft" : "m";
+                  return `${type}: ${context.formattedValue} ${label}`;
                 }
 
                 const value = context.dataset.data[context.dataIndex];
                 const duration = Duration.fromObject({ seconds: value });
-                return `${context.dataset.label}: ${duration.toFormat("h 'hr' mm 'min'")}`;
+                return `${type}: ${duration.toFormat("h 'hr' mm 'min'")}`;
               },
             },
           },
           legend: {
-            position: 'bottom',
+            position: "bottom",
             labels: {
               boxWidth: 6,
               usePointStyle: true,
-              color: 'white',
+              color: "white",
               padding: 15,
             },
           },
@@ -140,7 +154,7 @@ export default {
               display: false,
             },
             ticks: {
-              color: 'white',
+              color: "white",
             },
           },
           y: {
@@ -149,14 +163,14 @@ export default {
               display: false,
             },
             ticks: {
-              color: 'white',
+              color: "white",
               callback: (value, index, ticks) => {
-                if (this.field === 'distance') {
-                  const label = this.imperial ? 'mi' : 'km';
+                if (this.field === "distance") {
+                  const label = this.imperial ? "mi" : "km";
                   return formatNumber(value) + label;
                 }
-                if (this.field === 'elevation_gain') {
-                  const label = this.imperial ? 'ft' : 'm';
+                if (this.field === "elevation_gain") {
+                  const label = this.imperial ? "ft" : "m";
                   return formatNumber(value) + label;
                 }
 
@@ -168,18 +182,27 @@ export default {
         },
         onHover: (evt) => {
           const { chart } = evt;
-          const points = chart.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, true);
+          const points = chart.getElementsAtEventForMode(
+            evt,
+            "nearest",
+            { intersect: true },
+            true
+          );
 
           if (points.length) {
-            evt.native.target.style.cursor = 'pointer';
-          }
-          else {
-            evt.native.target.style.cursor = 'inherit';
+            evt.native.target.style.cursor = "pointer";
+          } else {
+            evt.native.target.style.cursor = "inherit";
           }
         },
         onClick: (evt) => {
           const { chart } = evt;
-          const points = chart.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, true);
+          const points = chart.getElementsAtEventForMode(
+            evt,
+            "nearest",
+            { intersect: true },
+            true
+          );
 
           if (points.length) {
             const firstPoint = points[0];
@@ -190,16 +213,17 @@ export default {
 
             this.chart.update();
           }
-        }
+        },
       },
     });
 
-    $('#recent-activities-chart-type').on('change', (evt) => {
+    $("#recent-activities-chart-type").on("change", (evt) => {
       this.field = evt.currentTarget.value;
       this.chart.data = this.calcChartData();
-      this.chart.options.scales.y.ticks['stepSize'] = this.field === 'duration' ? 3600 : null;
+      this.chart.options.scales.y.ticks["stepSize"] =
+        this.field === "duration" ? 3600 : null;
 
       this.chart.update();
     });
-  }
-}
+  },
+};
