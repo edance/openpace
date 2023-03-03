6f3faf910b746e710c2915e267895239fb27eac1
Evan Dancer <evancdancer@gmail.com>
2023-03-02 20:42:18 -0800

    Fix flaky test for activity_loader

--- test/factories/detailed_activity_factory.ex
+++ test/factories/detailed_activity_factory.ex
@@ -7,14 +7,22 @@ defmodule Squeeze.DetailedActivityFactory do
     quote do
       def detailed_activity_factory do
         timezone = "America/New_York"
+        now = Timex.now()
+
+        # Strava sends a datetime without a timezone for the start_date_local
+        local =
+          now
+          |> Timex.to_datetime(timezone)
+          |> Timex.format!("%FT%TZ", :strftime)
+          |> Timex.parse!("{ISO:Extended:Z}")
 
         %DetailedActivity{
-          id: sequence(:external_id, &(&1)),
+          id: sequence(:external_id, & &1),
           name: Enum.random(["Morning Run", "Afternoon Run", "Evening Run"]),
           distance: 5000.0,
-          moving_time: 1_200, # 20 minutes
-          start_date: Timex.now(),
-          start_date_local: Timex.to_datetime(Timex.now(), timezone),
+          moving_time: 1_200,
+          start_date: now,
+          start_date_local: local,
           map: %{summary_polyline: "ABCDEF"},
           type: "Run"
         }
