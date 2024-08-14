39e61f236e7715408150dbde3e2135a1ef4c3864
Evan Dancer <evancdancer@gmail.com>
2024-08-14 15:16:37 -0700

    Tailwind seems to be working

--- mix.exs
+++ mix.exs
@@ -121,8 +121,8 @@ defmodule Squeeze.Mixfile do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
-      # "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
       "assets.deploy": [
+        "tailwind default --minify",
         "cmd --cd assets bun install",
         "cmd --cd assets node build.js --deploy",
         "phx.digest"
