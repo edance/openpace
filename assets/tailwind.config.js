bfd5682be395c5131ed88d25e6c0f295253e6c44
Evan Dancer <evancdancer@gmail.com>
2024-08-15 13:35:16 -0700

    Working on light theme

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -16,6 +16,7 @@ module.exports = {
       }
     },
   },
+  darkMode: 'selector',
   plugins: [
     require('@tailwindcss/forms'),
     plugin(({addVariant}) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
