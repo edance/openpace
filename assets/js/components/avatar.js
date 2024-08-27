112ebf4694a2ce3cc3308c8c713e3b5b348d7477
Evan Dancer <evancdancer@gmail.com>
2024-08-27 13:37:37 -0700

    Fix avatar component when image not loaded

--- assets/js/components/avatar.js
+++ assets/js/components/avatar.js
@@ -1,18 +1,15 @@
-import { u } from 'umbrellajs';
-
 function init() {
-  u('.avatar img').each((image) => {
+  document.querySelectorAll('.avatar img').forEach((image) => {
     if (image.complete && image.naturalHeight === 0) {
-      image.classList.add("d-none");
+      image.classList.add("hidden");
     }
-  });
 
-  u('.avatar img').on('load', (x) => {
-    const image = x.target;
-
-    if (image.complete && image.naturalHeight === 0) {
-      image.classList.add("d-none");
-    }
+    image.addEventListener('load', function(x) {
+      const target = x.target;
+      if (target.complete && target.naturalHeight === 0) {
+        target.classList.add("hidden");
+      }
+    });
   });
 }
 
