d78f285a4755cbba3a68003b079c9f185e579e29
Evan Dancer <evancdancer@gmail.com>
2024-08-27 10:03:31 -0700

    More updates

--- lib/squeeze_web/components/core_components.ex
+++ lib/squeeze_web/components/core_components.ex
@@ -319,7 +319,7 @@ defmodule SqueezeWeb.CoreComponents do
         id={@id}
         name={@name}
         class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
+          "mt-2 block w-full rounded-lg text-gray-800 dark:text-white bg-white dark:bg-white/10 focus:ring-0 sm:text-sm sm:leading-6",
           "min-h-[6rem] phx-no-feedback:border-zinc-300 phx-no-feedback:focus:border-zinc-400",
           @errors == [] && "border-zinc-300 focus:border-zinc-400",
           @errors != [] && "border-rose-400 focus:border-rose-400"
