267bdd537bea940aa7190f3e4eb3338365f79d5d
Evan Dancer <evancdancer@gmail.com>
2024-09-03 10:44:02 -0700

    Add fit parsing in rust

--- lib/squeeze/rust_fit.ex
+++ lib/squeeze/rust_fit.ex
@@ -1,7 +1,11 @@
+defmodule Squeeze.FitRecord do
+  defstruct [:kind, :fields]
+end
+
 defmodule Squeeze.RustFit do
   use Rustler, otp_app: :squeeze, crate: "rust_fit"
 
   # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def parse_fit_file(_file_path), do: :erlang.nif_error(:nif_not_loaded)
 end
 
--- native/rust_fit/src/lib.rs
+++ native/rust_fit/src/lib.rs
@@ -1,6 +1,35 @@
-#[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
+use rustler::{Encoder, Env, Error, Term, NifStruct};
+use std::fs::File;
+use std::io::BufReader;
+use fitparser;
+
+#[derive(NifStruct)]
+#[module = "Squeeze.FitRecord"]
+struct FitRecord {
+    kind: String,
+    fields: Vec<(String, String)>,
+}
+
+#[rustler::nif(schedule = "DirtyCpu")]
+fn parse_fit_file<'a>(env: Env<'a>, file_path: String) -> Result<Term<'a>, Error> {
+    println!("Parsing FIT files using Profile version: {}", fitparser::profile::VERSION);
+    
+    let fp = File::open(file_path).map_err(|e| Error::Term(Box::new(e.to_string())))?;
+    let mut reader = BufReader::new(fp);
+    
+    let records = fitparser::from_reader(&mut reader)
+        .map_err(|e| Error::Term(Box::new(e.to_string())))?;
+
+    let fit_records: Vec<FitRecord> = records.into_iter().map(|record| {
+        FitRecord {
+            kind: record.kind().to_string(),
+            fields: record.fields().iter().map(|field| {
+                (field.name().to_string(), field.value().to_string())
+            }).collect(),
+        }
+    }).collect();
+
+    Ok(fit_records.encode(env))
 }
 
 rustler::init!("Elixir.Squeeze.RustFit");
