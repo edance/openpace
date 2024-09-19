dc8184d2fae5c82179cf6fd0dad70ebd49d93e3d
Evan Dancer <evancdancer@gmail.com>
2024-09-19 10:03:31 -0700

    Add styling to file input and progress bar

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,27 +1,49 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-let plugin = require('tailwindcss/plugin')
+let plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: [
-    './js/**/*.js',
-    '../lib/*_web.ex',
-    '../lib/*_web/**/*.*ex'
-  ],
+  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
   theme: {
     extend: {
       colors: {
-        'midnight': '#424660',
-      }
+        midnight: "#424660",
+      },
     },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [
-    require('@tailwindcss/forms'),
-    plugin(({addVariant}) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
-    plugin(({addVariant}) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
-    plugin(({addVariant}) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
-    plugin(({addVariant}) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
-  ]
-}
+    require("@tailwindcss/forms"),
+    plugin(({ addVariant }) =>
+      addVariant("phx-no-feedback", ["&.phx-no-feedback", ".phx-no-feedback &"])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-click-loading", [
+        "&.phx-click-loading",
+        ".phx-click-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-submit-loading", [
+        "&.phx-submit-loading",
+        ".phx-submit-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-change-loading", [
+        "&.phx-change-loading",
+        ".phx-change-loading &",
+      ])
+    ),
+    // Add variants for progress bars
+    plugin(function ({ addVariant }) {
+      addVariant("progress-unfilled", ["&::-webkit-progress-bar", "&"]);
+      addVariant("progress-filled", [
+        "&::-webkit-progress-value",
+        "&::-moz-progress-bar",
+        "&",
+      ]);
+    }),
+  ],
+};
