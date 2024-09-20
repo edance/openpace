8917610ee382e73fa8988607a4aa5583e45c03a4
Evan Dancer <evancdancer@gmail.com>
2024-09-20 11:19:44 -0700

    Update strava stream to get all information

--- lib/squeeze/activities/trackpoint.ex
+++ lib/squeeze/activities/trackpoint.ex
@@ -10,9 +10,12 @@ defmodule Squeeze.Activities.Trackpoint do
     field :cadence, :integer
     field :coordinates, :map
     field :distance, :float
+    field :grade_smooth, :float
     field :heartrate, :integer
     field :moving, :boolean
+    field :temp, :integer
     field :time, :integer
     field :velocity, :float
+    field :watts, :integer
   end
 end
