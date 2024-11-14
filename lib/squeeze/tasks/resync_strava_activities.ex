9d905bb6ec5fad3654d872d879aa19265cc954f4
Evan Dancer <evancdancer@gmail.com>
2024-11-14 13:14:31 -0800

    Add script to create race goals

--- lib/squeeze/tasks/resync_strava_activities.ex
+++ lib/squeeze/tasks/resync_strava_activities.ex
@@ -7,6 +7,7 @@ defmodule Squeeze.Tasks.ResyncStravaActivities do
   alias Squeeze.Accounts.Credential
   alias Squeeze.Repo
   alias Squeeze.Strava.HistoryLoader
+  alias Squeeze.Strava.Client
 
   @doc """
     Run the task
@@ -21,7 +22,28 @@ defmodule Squeeze.Tasks.ResyncStravaActivities do
     |> Enum.each(&resync_activities/1)
   end
 
+  @doc """
+  Checks if the credential still has valid access to Strava
+  Returns {:ok, athlete} if valid, {:error, reason} if not
+  """
+  def verify_access(%Credential{} = credential) do
+    client = Client.new(credential)
+
+    case Strava.Athletes.get_logged_in_athlete(client) do
+      {:ok, athlete} -> {:ok, athlete}
+      {:error, %{status: 401}} -> {:error, :unauthorized}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
   defp resync_activities(%Credential{} = credential) do
-    HistoryLoader.load_all(credential)
+    case verify_access(credential) do
+      {:ok, _athlete} ->
+        HistoryLoader.load_all(credential)
+
+      {:error, reason} ->
+        IO.puts("Failed to sync activities for user: #{credential.user.email}, reason: #{reason}")
+        {:error, reason}
+    end
   end
 end
