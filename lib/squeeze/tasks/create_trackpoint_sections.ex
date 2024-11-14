1c4021572c0abc0e5c9c3a687a858ca8d38cda48
Evan Dancer <evancdancer@gmail.com>
2024-11-14 10:59:25 -0800

    Update trackpoint section script
    Skip the trackpoint sections that have already been generated

--- lib/squeeze/tasks/create_trackpoint_sections.ex
+++ lib/squeeze/tasks/create_trackpoint_sections.ex
@@ -7,8 +7,10 @@ defmodule Squeeze.Tasks.CreateTrackpointSections do
 
   import Ecto.Query, warn: false
   alias Squeeze.Activities
-  alias Squeeze.Activities.TrackpointSet
+  alias Squeeze.Activities.{TrackpointSection, TrackpointSet}
   alias Squeeze.Repo
+
+  require Logger
 
   @doc """
   Starts the batch processing of all TrackpointSets.
@@ -21,11 +23,22 @@ defmodule Squeeze.Tasks.CreateTrackpointSections do
       {:ok}
   """
   def run do
-    query = from(t in TrackpointSet)
-    process_in_batches(query)
+    query =
+      from t in TrackpointSet,
+        left_join: s in TrackpointSection,
+        on: t.activity_id == s.activity_id,
+        where: is_nil(s.id)
+
+    total_count = Repo.aggregate(query, :count, :id)
+
+    Logger.info("Processing #{total_count} TrackpointSets to create TrackpointSections")
+
+    result = process_in_batches(query, total_count)
+    System.stop(0)
+    result
   end
 
-  defp process_in_batches(query, page \\ 0) do
+  defp process_in_batches(query, total_count, page \\ 0, processed_count \\ 0) do
     sets =
       query
       |> limit(100)
@@ -33,12 +46,19 @@ defmodule Squeeze.Tasks.CreateTrackpointSections do
       |> Repo.all()
 
     if sets == [] do
+      Logger.info("Completed processing of TrackpointSets to create TrackpointSections")
       {:ok}
     else
+      new_processed_count = processed_count + length(sets)
+
+      Logger.info(
+        "Processing batch #{page + 1} - #{round(new_processed_count / total_count * 100)}%"
+      )
+
       sets
       |> Enum.each(&Activities.create_trackpoint_sections/1)
 
-      process_in_batches(query, page + 1)
+      process_in_batches(query, total_count, page + 1, new_processed_count)
     end
   end
 end
